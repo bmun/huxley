45dc8dbf1b221e2b0fff7e70f3537e57083c5678
Kunal Mehta <k.mehta@berkeley.edu>
2012-07-13 20:02:13 -0700

    Cleaned up the change password JS file.

--- cms/static/js/change-password.js
+++ cms/static/js/change-password.js
@@ -1,57 +1,78 @@
+function successLabel() {
+    return '<label class="success"> Success! </label>';
+}
+
+function errorLabel(message) {
+    return '<label class="error">' + message + '</label>';
+}
+
 $(function(){
 
-$(document).on("click", "#newpassword", function(e){
-    if($("#changepassword").is(":visible")){
-        $("#changepassword").slideUp(function(){
-            $("#changepassword div.input input").val('');
-            $("#changepassword #message").hide();
-        });
-        
-    }
-    else{
-        $("#changepassword").slideDown();
-    }
-    return false;
-});
-
-$(document).click(function(){
-    if($("#changepassword").is(":visible")){
-        $("#changepassword").slideUp(function(){
-            $("#changepassword div.input input").val('');
-            $("#changepassword #message").hide();
-        });
-    }
-});
-
-$(document).on("click", ".changepassword", function(e){
-    e.stopPropagation();
-});
-
-
-$(document).on("submit", "#changepasswordform", function(){
-	var uri = $(this).attr("action");
-    $.post(uri, $(this).serializeArray(), function(data){
-        if(data == 'OK'){
-        	$("#changepassword input[type=password]").val("");
-        	if ($("#changepassword #message").is(":visible")) {
-        		$("#changepassword #message").hide().html('<label class="success"> Success! </label>').fadeIn(250);
-        	} else {
-        		$("#changepassword #message").hide().html('<label class="success"> Success! </label>').slideDown(250);
-        	}
-            $("#changepassword").delay(750).slideUp();
-            $("#changepassword #message").delay(750).slideUp();
+    $(document).on("click", "#newpassword", function(e) {
+        if ($("#changepassword").is(":visible")) {
+            $("#changepassword").slideUp(function() {
+                $("#changepassword div.input input").val('');
+                $("#changepassword #message").hide();
+            });
+        } else {
+            $("#changepassword").slideDown();
         }
-        else{
-            if($("#changepassword #message").is(":visible")){
-                $("#changepassword #message").hide().html('<label class="error">' + data + '</label>').fadeIn(250);
-            }
-            else{
-                $("#changepassword #message").hide().html('<label class="error">' + data + '</label>').slideDown(250);
-            }
-            $("#changepassword").effect("shake", { direction:"left", times:2, distance:2 }, 50);
+        
+        return false;
+    });
+
+    $(document).click(function() {
+        if ($("#changepassword").is(":visible")) {
+            $("#changepassword").slideUp(function() {
+                $("#changepassword div.input input").val('');
+                $("#changepassword #message").hide();
+            });
         }
     });
-    return false;
-});
 
+    $(document).on("click", ".changepassword", function(e) {
+        e.stopPropagation();
+    });
+
+
+    $(document).on("submit", "#changepasswordform", function() {
+        var uri = $(this).attr("action");
+        $.post(uri, $(this).serializeArray(), function(data) {
+            if (data == 'OK') {
+                $("#changepassword input[type=password]").val("");
+                if ($("#changepassword #message").is(":visible")) {
+                    $("#changepassword #message")
+                        .hide()
+                        .html(successLabel())
+                        .fadeIn(250);
+                } else {
+                    $("#changepassword #message")
+                        .hide()
+                        .html(successLabel())
+                        .slideDown(250);
+                }
+                
+                $("#changepassword").delay(750).slideUp();
+                $("#changepassword #message").delay(750).slideUp();
+                return false;
+            } else if ($("#changepassword #message").is(":visible")) {
+                $("#changepassword #message")
+                    .hide()
+                    .html(errorLabel(data))
+                    .fadeIn(250);
+            } else {
+                $("#changepassword #message")
+                    .hide()
+                    .html(errorLabel(data))
+                    .slideDown(250);
+            }
+            $("#changepassword").effect(
+                "shake",
+                { direction:"left", times:2, distance:2 },
+                50
+            );
+        });
+        
+        return false;
+    });
 });
