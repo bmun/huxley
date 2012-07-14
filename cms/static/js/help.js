72126b131a2d52a3008e97300a73ff8e76decd0b
Kunal Mehta <k.mehta@berkeley.edu>
2012-07-13 20:26:01 -0700

    Tidied up help.js.

--- cms/static/js/help.js
+++ cms/static/js/help.js
@@ -1,17 +1,20 @@
-$(function(){    
+$(function() {
+
     // Accordion Stuff
-    // Tutorial courtesy of: http://www.sohtanaka.com/web-design/simple-accordion-w-css-and-jquery/
+    // Tutorial courtesy of:
+    // http://www.sohtanaka.com/web-design/simple-accordion-w-css-and-jquery/
     $(document).on("click", ".acc_topic", function(){
         if ($(this).next().is(':hidden')) {
-                $(".acc_topic").removeClass('active');
-                $(".acc_topic").next().children('p').fadeOut(150);
-                $(".acc_topic").next().slideUp(200);
-                $(this).next().children('p').fadeIn(150);
-                $(this).toggleClass('active').next().slideDown(200);
+            $(".acc_topic").removeClass('active');
+            $(".acc_topic").next().children('p').fadeOut(150);
+            $(".acc_topic").next().slideUp(200);
+            $(this).next().children('p').fadeIn(150);
+            $(this).toggleClass('active').next().slideDown(200);
         } else {
-                $(this).next().children('p').fadeOut(150);
-                $(this).removeClass('active').next().slideUp(200);
+            $(this).next().children('p').fadeOut(150);
+            $(this).removeClass('active').next().slideUp(200);
         }
-        return false; // Think this overrides default behavior?
+        
+        return false;
     });
 });
