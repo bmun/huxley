6b073ee63290dacddbac85634d2bbfb28ec1a02a
Kunal Mehta <k.mehta@berkeley.edu>
2012-07-13 19:31:51 -0700

    Touched up auth.js to conform to style guide.

--- cms/static/js/auth.js
+++ cms/static/js/auth.js
@@ -1,31 +1,29 @@
-$(function(){
-	$("#container").delegate("#loginform", "submit", function(){
-		var credentials = $(this).serializeArray();
-		var uri = $(this).attr("action");
-		
-		$.post(uri, credentials, function(data){
-			if(data.success === true){
-				ContentManager.onLoginLogout(data.redirect, 250);
-			}
-			else{
-				$("#errorcontainer").hide().html('<label class="error">' + data.error + '</label>').fadeIn(250);
-				$("#app").effect("shake", { direction:"up", times:2, distance:2 }, 50);
-			}
-		});
-
-		return false;
-	});
-	
-	$("#container").delegate("#logout", "click", function(){
-		var uri = $(this).attr("href");
-     	$.get(uri, function(redirect) {
-           	$("#header").slideUp(250, function(){
-				$("#headerwrapper").slideUp(250, function() {
-      				ContentManager.onLoginLogout(redirect, 0);
-				});
-			});
-          });
+$(function() {
+    $(document).on("submit", "#loginform", function() {
+        var credentials = $(this).serializeArray();
+        var uri = $(this).attr("action");
+        $.post(uri, credentials, function(data) {
+            if(data.success === true) {
+                ContentManager.onLoginLogout(data.redirect, 250);
+            } else {
+                $("#errorcontainer").hide().html('<label class="error">' + data.error + '</label>').fadeIn(250);
+                $("#app").effect("shake", { direction:"up", times:2, distance:2 }, 50);
+            }
+        });
         
-     	return false;
-	});
+        return false;
+    });
+	
+    $(document).on("click", "#logout", function() {
+        var uri = $(this).attr("href");
+        $.get(uri, function(redirect) {
+            $("#header").slideUp(250, function() {
+                $("#headerwrapper").slideUp(250, function() {
+                    ContentManager.onLoginLogout(redirect, 0);
+                });
+            });
+        });
+        
+        return false;
+    });
 });
