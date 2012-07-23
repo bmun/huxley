cd89f2c35ac7454bd2ef6c38dbf0580dbc5f4abf
Kunal Mehta <k.mehta@berkeley.edu>
2012-07-22 20:30:34 -0700

    Heavily refactored the forgot_password view and made it PEP8 compliant.

--- core/views.py
+++ core/views.py
@@ -1,20 +1,18 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-from django.core.context_processors import csrf
 from django.core.urlresolvers import reverse
 from django.core.validators import email_re
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotAllowed, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseRedirect, \
+                        HttpResponseNotAllowed
 from django.shortcuts import render_to_response
-from django.template import Context, RequestContext
+from django.template import RequestContext
 from django.utils import simplejson
 
 from cms.models import *
 
-import datetime
-import sys
-import traceback
 import re
 
+# Logs in a user or renders the login template.
 def login_user(request):
     if request.method == "POST":    
         username = request.POST.get('username')
@@ -23,7 +21,7 @@ def login_user(request):
         error = ""
         
         # Determine if the login is valid.
-        if len(username) == 0 or len(password) == 0:
+        if not (username and password):
             error = "Woops! One or more of the fields is blank."
         elif user is None:
             error = "Sorry! The login you provided was invalid."
@@ -51,9 +49,11 @@ def login_user(request):
         else:
             return HttpResponseRedirect(reverse('index'))
         
-    return render_to_response('auth.html')
+    c = RequestContext(request)
+    return render_to_response('auth.html', c)
 
 
+# Logs out the current user.
 def logout_user(request):
     logout(request)
     if request.is_ajax():
@@ -62,90 +62,69 @@ def logout_user(request):
         return HttpResponseRedirect(reverse('index'))
 
 
+# Attempts to change current user's password.
 def change_password(request):
     if not request.method == "POST":
         return HttpResponseNotAllowed(["POST"])
         
-    oldpassword = request.POST.get('oldpassword')
-    newpassword = request.POST.get('newpassword')
-    newpassword2 = request.POST.get('newpassword2')
+    oldpass = request.POST.get('oldpassword')
+    newpass = request.POST.get('newpassword')
+    newpass2 = request.POST.get('newpassword2')
     
     if not request.user.is_authenticated():
         return HttpResponse(status=401)
-    elif not (oldpassword or newpassword or newpassword2):
+    elif not (oldpass or newpass or newpass2):
         return HttpResponse("One or more fields is blank.")
-    elif newpassword != newpassword2:
+    elif newpass != newpass2:
         return HttpResponse("New passwords must match.")
-    elif len(newpassword) < 6:
+    elif len(newpass) < 6:
         return HttpResponse("New password must be at least 6 characters long.")
-    elif not re.match("^[A-Za-z0-9\_\.!@#\$%\^&\*\(\)~\-=\+`\?]+$", newpassword):
-        return HttpResponse("New password must have only alphanumeric characters and symbols (above letters)")
-    elif not request.user.check_password(oldpassword):
+    elif not re.match("^[A-Za-z0-9\_\.!@#\$%\^&\*\(\)~\-=\+`\?]+$", newpass):
+        return HttpResponse("New password must have only alphanumeric "
+                            "characters and symbols (above letters)")
+    elif not request.user.check_password(oldpass):
         return HttpResponse("Incorrect password.")
     else:
-        request.user.set_password(newpassword)
+        request.user.set_password(newpass)
         request.user.save();
         return HttpResponse('OK')
 
 
+# Resets a user's password.
 def forgot_password(request):
-        if request.method == 'POST':
-                user = request.POST.get('username')
-                input_email = request.POST.get('email')
-                
-                if len(user) == 0 and len(input_email) == 0:
-                        return HttpResponse("You must fill at least one of the fields!")
-                
-                if len(user) > 0 and len(input_email) > 0:
-                        # Double check against database, make sure email matches that username
-                        saidUser = None
-                        try:
-                                saidUser = User.objects.get(username__exact = user)
-                        except:
-                                return HttpResponse("No such user in the database!")
-                                
-                        if input_email == saidUser.email:
-                                # Email matches user; password reset
-                                newPass = User.objects.make_random_password(length = 8,allowed_chars='abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789')
-                                saidUser.set_password(newPass)
-                                saidUser.save()
-                                saidUser.email_user("BMUN CMS Password Reset", "Your password has been reset to " + newPass + ".", "password@cms.bmun.net")
-                                print "Email sent! (Theoretically)"
-                        else:
-                                return HttpResponse("Email does not match this user in database!")
-                                
-                else:
-                        # If email: grab email and do the password stuffs.
-                        if len(input_email) > 0:
-                                saidUser = None
-                                try:
-                                        saidUser = User.objects.get(email = input_email)
-                                except:
-                                        return HttpResponse("No user matches that email!")
-                                
-                                # Password Reset
-                                newPass = User.objects.make_random_password(length = 8,allowed_chars='abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789')
-                                saidUser.set_password(newPass)
-                                saidUser.save()
-                                saidUser.email_user("BMUN CMS Password Reset", "Your password has been reset to " + newPass + ".", "password@cms.bmun.net")
-                                print "Email sent! (Theoretically)"
-                                
-                        else:
-                                # Else: Check if user is in database first
-                                saidUser = None
-                                try:
-                                        saidUser = User.objects.get(username__exact = user)
-                                except:
-                                        return HttpResponse("This user doesn't exist!")
-                                
-                                # Password Reset
-                                newPass = User.objects.make_random_password(length = 8,allowed_chars='abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789')
-                                saidUser.set_password(newPass)
-                                saidUser.save()
-                                saidUser.email_user("BMUN CMS Password Reset", "Your password has been reset to " + newPass + ".", "password@cms.bmun.net")
-                                print "Email sent! (Theoretically)"
-        else:
-                return render_to_response('forgot.html', context_instance = RequestContext(request))
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        input_email = request.POST.get('email')
+        
+        if not (username or input_email):
+            return HttpResponse("You must fill at least one of the fields.")
+        
+        user = None
+        try:
+            if username and input_email:
+                user = User.objects.get(username__exact=username,
+                                        email__exact=input_email)
+            elif input_email:
+                user = User.objects.get(email__exact=input_email)
+            else:
+                user = User.objects.get(username__exact=username)
+        except:
+            return HttpResponse("Sorry! We couldn't find a user matching the "
+                                "given username and/or email.")
+        
+        new_pass = User.objects.make_random_password(length=8)
+        user.set_password(new_pass)
+        user.save()
+        saidUser.email_user("Huxley Password Reset",
+                            "Your password has been reset to %s." % new_pass,
+                            "password@cms.bmun.net")
+        
+        return HttpResponse()
+    else:
+        return render_to_response('forgot.html',
+                                  context_instance=RequestContext(request))
 
+
+# Renders the "About" page.
 def about(request):
         return render_to_response('about.html')
