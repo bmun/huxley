67efd5d4ab7565f432305aa4da4f4dbd706aae83
Kunal Mehta <k.mehta@berkeley.edu>
2013-01-23 19:29:08 -0500

    Added an exception handler for the conference not existing in the context processor.

--- core/context_processors.py
+++ core/context_processors.py
@@ -2,7 +2,13 @@
 # Use of this source code is governed by a BSD License found in README.md.
 
 from core.models import Conference
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpRequest
 
 def conference(request):
-	return {'conference' : Conference.objects.latest()}
+	try:
+		conference =  Conference.objects.latest()
+	except ObjectDoesNotExist:
+		conference = None
+
+	return {'conference' : conference}
