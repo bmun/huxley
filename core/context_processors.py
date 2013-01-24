797e55f21d9a586a4938b36e8e3f8e82ea75a72e
Kunal Mehta <k.mehta@berkeley.edu>
2013-01-23 19:47:49 -0500

    Added the conference query to middleware, and modified the context_processor to access the conference property of the request object.

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,14 +1,7 @@
 # Copyright (c) 2011-2013 Kunal Mehta. All rights reserved.
 # Use of this source code is governed by a BSD License found in README.md.
 
-from core.models import Conference
-from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpRequest
 
 def conference(request):
-	try:
-		conference =  Conference.objects.latest()
-	except ObjectDoesNotExist:
-		conference = None
-
-	return {'conference' : conference}
+	return {'conference' : request.conference}
--- settings.py
+++ settings.py
@@ -103,6 +103,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'core.middleware.LatestConferenceMiddleware',
 )
 
 ROOT_URLCONF = 'huxley.urls'
