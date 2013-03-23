4476b00c2ab519de17c1cce1c6f2511767a8ffa5
Kunal Mehta <k.mehta@berkeley.edu>
2013-03-23 01:46:29 -0700

    Added copyright notices to some python files.

--- huxley/chairs/models.py
+++ huxley/chairs/models.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2011-2013 Kunal Mehta. All rights reserved.
+# Use of this source code is governed by a BSD License found in README.md.
+
 from django.db import models
 
 # Create your models here.
