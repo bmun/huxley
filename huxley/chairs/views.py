952292481b07968c793ed2b93ec7ccd4b340d32f
Kunal Mehta <k.mehta@berkeley.edu>
2013-03-23 01:46:29 -0700

    Added copyright notices to some python files.

--- huxley/chairs/views.py
+++ huxley/chairs/views.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2011-2013 Kunal Mehta. All rights reserved.
+# Use of this source code is governed by a BSD License found in README.md.
+
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse, HttpResponseNotFound
