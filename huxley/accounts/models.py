6e27796cf20dda07a77a5f5a7d62ff63551c02b8
Kunal Mehta <k.mehta@berkeley.edu>
2013-04-02 14:58:07 -0700

    Added copyright notices.

--- huxley/accounts/models.py
+++ huxley/accounts/models.py
@@ -1,3 +1,2 @@
-from django.db import models
-
-# Create your models here.
+# Copyright (c) 2011-2013 Kunal Mehta. All rights reserved.
+# Use of this source code is governed by a BSD License found in README.md.
