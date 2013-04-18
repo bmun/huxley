dd111a1820b1b2d5f5847c81338caba4337fe86a
Kunal Mehta <k.mehta@berkeley.edu>
2013-04-17 22:30:41 -0700

    Added a render_json function to huxley.shortcuts, used it in accounts.views.

--- huxley/shortcuts.py
+++ huxley/shortcuts.py
@@ -1,8 +1,10 @@
 # Copyright (c) 2011-2013 Kunal Mehta. All rights reserved.
 # Use of this source code is governed by a BSD License found in README.md.
 
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.utils import simplejson
 
 from itertools import izip_longest
 
@@ -10,6 +12,10 @@ def render_template(request, template, context=None):
     """ Wrapper around render_to_response that sets context_instance. """
     return render_to_response(template, context, 
                               context_instance=RequestContext(request))
+
+def render_json(data):
+	""" Returns an HttpResponse object containing json-encoded data. """
+	return HttpResponse(simplejson.dumps(data), mimetype='application/json')
 
 def pairwise(iterable):
     """ s -> (s0, s1), (s2, s3), ... """
