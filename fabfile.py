72f1213ca505637c56c5f3d835bd929ca4d1a4a3
Kunal Mehta <k.mehta@berkeley.edu>
2013-06-22 19:36:10 -0700

    Added fabfile with automated git workflow.

---
+++ fabfile.py
@@ -0,0 +1,43 @@
+from fabric.api import run, local
+from fabric.contrib.console import confirm
+
+def feature(branch_name=''):
+    if not branch_name:
+        print "No branch name given. Usage: fab feature:<branch_name>"
+        return
+    local('git checkout -tb %s' % branch_name)
+
+def update(remote='upstream'):
+    local('git fetch %s' % remote);
+    local('git rebase %s/master' % remote)
+
+def review():
+    print "Updating your current branch..."
+    update()
+    
+    print "Pushing to remote topic branch..."
+    branch_name = local('git rev-parse --abbrev-ref HEAD', capture=True)
+    local('git push origin %s' % branch_name)
+    
+    print "Issuing pull request..."
+    local('hub pull-request')
+
+def finish():
+    branch_name = local('git rev-parse --abbrev-ref HEAD', capture=True)
+    if branch_name == 'master':
+        print "On branch master. You probably don't want to do this."
+        return
+    
+    if not confirm("This will delete your local and remote topic branch. "
+                   "Make sure your pull request has been merged or closed. "
+                   "Are you sure you want to finish this branch?"):
+        print "Aborting."
+        return
+
+    print "Deleting remote branch..."
+    local('git push origin :%s' % branch_name)
+
+    print "Deleting local branch..."
+    local('git checkout master')
+    local('git branch -D %s' % branch_name)
+
