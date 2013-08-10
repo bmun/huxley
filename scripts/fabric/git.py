16f3df2bf3e7cf625531e93bbf08dae56216f441
Kunal Mehta <k.mehta@berkeley.edu>
2013-08-10 03:30:15 -0400

    (#79) Updated fabric git commands to rebase properly.

---
+++ scripts/fabric/git.py
@@ -0,0 +1,55 @@
+# Copyright (c) 2011-2013 Kunal Mehta. All rights reserved.
+# Use of this source code is governed by a BSD License found in README.md.
+
+from fabric.api import hide, local
+from fabric.colors import red
+
+def master_guard(fn):
+    def guarded_fn(*args, **kwargs):
+        if current_branch() == 'master':
+            print red('Currently on master branch. '
+                      'You probably don\'t want to do this. Aborting.')
+            return
+        fn(*args, **kwargs)
+    return guarded_fn
+
+def new_branch(branch_name, remote='upstream'):
+    local('git fetch upstream')
+    local('git checkout -tb %s %s/master' % (branch_name, remote))
+
+def current_branch():
+    with hide('running'):
+        return local('git rev-parse --abbrev-ref HEAD', capture=True)
+
+def remote_branch_exists(branch_name=None, remote='upstream'):
+    branch_name = branch_name or current_branch()
+    with hide('running'):
+        heads_list = local('git ls-remote --heads %s' % remote, capture=True).split('\n')
+    full_name = 'refs/heads/%s' % branch_name
+    return any(full_name in head for head in heads_list)
+
+def commits_ahead(remote='upstream'):
+    with hide('running'):
+        commit_list = local('git rev-list --left-right %s/master...HEAD' % remote, capture=True)
+    return len(commit_list.split('\n'))
+
+def pull(rebase=True):
+    rebase_flag = '--rebase' if rebase else ''
+    local('git pull %s' % rebase_flag)
+
+@master_guard
+def push(branch_name):
+    local('git push origin %s --force' % branch_name)
+
+@master_guard
+def cleanup(branch_name=None):
+    branch_name = branch_name or current_branch()
+
+    print "Deleting remote branch..."
+    local('git push origin :%s' % branch_name)
+
+    print "Deleting local branch..."
+    local('git checkout master')
+    local('git branch -D %s' % branch_name)
+
+    return branch_name
