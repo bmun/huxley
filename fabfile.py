198a1b745af5fbe6c5a31fce2f8ad1e840135b8a
Kunal Mehta <k.mehta@berkeley.edu>
2013-08-10 03:30:15 -0400

    (#79) Updated fabric git commands to rebase properly.

--- fabfile.py
+++ fabfile.py
@@ -2,55 +2,36 @@
 # Use of this source code is governed by a BSD License found in README.md.
 
 from fabric.api import run, local
+from fabric.colors import green, red, yellow
 from fabric.contrib.console import confirm
+from scripts.fabric import git
 
-def update(remote='upstream'):
-    print "Updating your local branch..."
-    local('git fetch %s' % remote);
-    local('git rebase %s/master' % remote)
-
-def feature(branch_name=''):
+def feature(branch_name=None):
     if not branch_name:
-        print "No branch name given. Usage: fab feature:<branch_name>"
+        print red('No branch name given. Usage: fab feature:<branch_name>')
         return
-    local('git checkout -tb %s' % branch_name)
-    update()
+    git.new_branch(branch_name)
+
+def update():
+    print 'Updating your local branch...'
+    git.pull()
 
 def submit():
-    update()
-    _push_to_remote_topic_branch()
-    
-    print "Push to remote branch successful. Go to github.com to issue a pull request."
-
-def revise():
-    _push_to_remote_topic_branch()
+    first_submission = not git.remote_branch_exists()
+    git.pull()
+    git.push()
+    if first_submission:
+        print green('Push to remote branch successful. '
+                    'Go to github.com to issue a pull request.')
+    else:
+        print green('Pull request sucessfully updated.')
 
 def finish():
-    branch_name = local('git rev-parse --abbrev-ref HEAD', capture=True)
-    if branch_name == 'master':
-        print "On branch master. You probably don't want to do this."
-        return
-    
-    if not confirm("This will delete your local and remote topic branch. "
-                   "Make sure your pull request has been merged or closed. "
-                   "Are you sure you want to finish this branch?"):
-        print "Aborting."
+    prompt = yellow('This will delete your local and remote topic branches. '
+                    'Make sure your pull request has been merged or closed. '
+                    'Are you sure you want to finish this branch?')
+    if not confirm(prompt):
+        print red('Aborting.')
         return
 
-    print "Deleting remote branch..."
-    local('git push origin :%s' % branch_name)
-
-    print "Deleting local branch..."
-    local('git checkout master')
-    local('git branch -D %s' % branch_name)
-
-
-def _push_to_remote_topic_branch():
-    print "Pushing to remote topic branch..."
-    branch_name = local('git rev-parse --abbrev-ref HEAD', capture=True)
-    local('git push origin %s' % branch_name)
-
-
-def static():
-    local('python manage.py collectstatic --noinput')
-
+    print green('Branch %s successfully cleaned up.' % git.cleanup())
