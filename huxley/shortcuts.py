3b0f3ccff78d2e7ba542cf9b22da545add92b5de
Kunal Mehta <k.mehta@berkeley.edu>
2013-09-05 10:35:08 -0700

    Update docstrings in shortcuts.py.

--- huxley/shortcuts.py
+++ huxley/shortcuts.py
@@ -9,15 +9,15 @@ from django.utils import simplejson
 from itertools import izip_longest
 
 def render_template(request, template, context=None):
-    """ Wrapper around render_to_response that sets context_instance. """
+    '''Wrap render_to_response with the context_instance argument set.'''
     return render_to_response(template, context, 
                               context_instance=RequestContext(request))
 
 def render_json(data):
-	""" Returns an HttpResponse object containing json-encoded data. """
-	return HttpResponse(simplejson.dumps(data), mimetype='application/json')
+    '''Return an HttpResponse object containing json-encoded data.'''
+    return HttpResponse(simplejson.dumps(data), mimetype='application/json')
 
 def pairwise(iterable):
-    """ s -> (s0, s1), (s2, s3), ... """
+    '''Group the elements of the given interable into 2-tuples.'''
     i = iter(iterable)
     return izip_longest(i, i)
