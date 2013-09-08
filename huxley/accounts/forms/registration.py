eaa807843499c7de9f3c3e73e59ab24e3f1de66a
Kunal Mehta <k.mehta@berkeley.edu>
2013-09-08 14:22:16 -0700

    Fixed clientside registration validation checks.

--- huxley/accounts/forms/registration.py
+++ huxley/accounts/forms/registration.py
@@ -33,9 +33,9 @@ class RegistrationForm(forms.Form):
     school_country  = forms.CharField(label="Country", initial='United States of America', widget=forms.TextInput(attrs={'disabled': True, 'placeholder': 'Country'}), required=False)
 
     program_type        = forms.ChoiceField(label="What category best describes your program?", widget=forms.RadioSelect, choices=School.PROGRAM_TYPE_OPTIONS, initial=School.TYPE_CLUB)
-    times_attended      = forms.IntegerField(label="Number of BMUN Sessions Attended", widget=forms.TextInput(attrs={'class':'required IntegersOnly', 'placeholder': 'Number of BMUN Sessions Attended'}))
-    min_delegation_size = forms.IntegerField(label="Minimum Delegation Size", widget=forms.TextInput(attrs={'class':'required IntegersOnly', 'placeholder': 'Minimum Delegation Size'}))
-    max_delegation_size = forms.IntegerField(label="Maximum Delegation Size", widget=forms.TextInput(attrs={'class':'required IntegersOnly', 'placeholder': 'Maximum Delegation Size'}))
+    times_attended      = forms.IntegerField(label="Number of BMUN Sessions Attended", widget=forms.TextInput(attrs={'class':'required positive-integer', 'placeholder': 'Number of BMUN Sessions Attended'}))
+    min_delegation_size = forms.IntegerField(label="Minimum Delegation Size", widget=forms.TextInput(attrs={'class':'required positive-integer', 'placeholder': 'Minimum Delegation Size'}))
+    max_delegation_size = forms.IntegerField(label="Maximum Delegation Size", widget=forms.TextInput(attrs={'class':'required positive-integer', 'placeholder': 'Maximum Delegation Size'}))
 
     primary_name    = forms.CharField(label="Name", widget=forms.TextInput(attrs={'class':'required', 'placeholder': 'Name'}))
     primary_email   = forms.EmailField(label="Email", widget=forms.TextInput(attrs={'class':'required email', 'placeholder': 'Email'}))
--- huxley/accounts/static/js/accounts/registration.js
+++ huxley/accounts/static/js/accounts/registration.js
@@ -1,115 +1,114 @@
 $(function() {
 
-    // Show/hide Country Field based on American/International
-    // Change validation rules too
-    $(document).on("click", "#registration .international-check", function() {
-        if ($(this).val() == "international") {
-            $('#country-row input')
+    // Show/hide the Country field based on the school's location.
+    $(document).on('click', '#registration .international-check', function() {
+        if ($(this).val() == 'location/international') {
+            $('#id_school_country')
                 .attr('disabled', false)
                 .attr('value', null)
                 .addClass('required');
-            $(".phoneVal").removeClass('phoneNum').addClass('intPhone');
-            $("#id_SchoolState").removeClass('required');
+            $('.phoneVal').removeClass('phoneNum').addClass('intPhone');
+            $('#id_school_state').removeClass('required');
         } else {
-            $('#country-row input')
+            $('#id_school_country')
                 .attr('disabled', true)
                 .attr('value', 'United States of America')
                 .removeClass('required');
-            $(".phoneVal").removeClass('intPhone').addClass('phoneNum');
-            $("#id_SchoolState").addClass('required');
+            $('.phoneVal').removeClass('intPhone').addClass('phoneNum');
+            $('#id_school_state').addClass('required');
         }
     });
 
-    // Integer-Only Check
-    $.validator.addMethod("IntegersOnly", function(value, element) {
+    // Nonnegative number.
+    $.validator.addMethod('positive-integer', function(value, element) {
         return (this.optional(element))
             ? (/^$/i.test(value) || /^[0-9]+$/i.test(value))
             : /^[0-9]+$/i.test(value);
-    }, "Please enter a positive number.");
+    }, 'Please enter a number.');
 
-    // Zip Code Check
-    // Allow 0-9, space, dash
-    $.validator.addMethod("zip", function(value, element) {
+    // Zip code: allow 0-9, space, dash
+    $.validator.addMethod('zip', function(value, element) {
         return (this.optional(element))
             ? (/^$/i.test(value) || /^[0-9\s\-]+$/i.test(value))
             : /^[0-9\s\-]+$/i.test(value);
-    }, "Zip codes may only contain numbers, spaces, and dashes.");
+    }, 'Zip codes may only contain numbers, spaces, and dashes.');
 
-    // U.S. Phone Number Check
-    $.validator.addMethod("phoneNum", function(value, element){
-        if (this.optional(element)) {
-            return /^$/i.test(value) || /^\(?([0-9]{3})\)?\s([0-9]{3})-([0-9]{4})(\sx[0-9]{1,5})?$/i.test(value);
-        } else {
-            return /^\(?([0-9]{3})\)?\s([0-9]{3})-([0-9]{4})(\sx[0-9]{1,5})?$/i.test(value);
+    // U.S. phone number.
+    $.validator.addMethod('phoneNum', function(value, element){
+        var PHONE_REGEX =
+            /^\(?([0-9]{3})\)?\s([0-9]{3})-([0-9]{4})(\sx[0-9]{1,5})?$/i;
+        if (!PHONE_REGEX.test(value)) {
+            return this.optional(element) && /^$/i.test(value);
         }
-    }, "Please enter a valid phone number.");
+        return true;
+    }, 'Please enter a valid phone number.');
 
-    // International Phone Number
-    $.validator.addMethod("intPhone", function(value, element) {
+    // International phone number.
+    $.validator.addMethod('intPhone', function(value, element) {
         return (this.optional(element))
             ? (/^$/i.test(value) || /^[0-9\-x\s\+\(\)]+$/i.test(value))
             : /^[0-9\-x\s\+\(\)]+$/i.test(value);
-    }, "Please enter a valid phone number.");
+    }, 'Please enter a valid phone number.');
 
-    // Username Check
-    $.validator.addMethod("username", function(value, element) {
+    // Username characters.
+    $.validator.addMethod('username', function(value, element) {
         return this.optional(element) || /^[A-Za-z0-9\_]+$/i.test(value);
-    }, "Usernames may only contain letters, numbers, and underscores.");
+    }, 'Usernames may only contain letters, numbers, and underscores.');
 
-    // Unique username check.
-    $.validator.addMethod("uniqueUser", function(value, element) {
+    // Username uniqueness.
+    $.validator.addMethod('uniqueUser', function(value, element) {
         var unique = false;
         $.ajax({
             type: 'GET',
             async: false,
             url: 'uniqueuser/',
-            data: {'username' : $("#registration input.username").val()},
+            data: {'username' : $('#registration input.username').val()},
             success: function(data, status, jq) {
-                unique = (status == "success");
+                unique = (status == 'success');
             }
         });
         return unique;
-    }, "This username is taken. :(");
+    }, 'We\re sorry, this username is taken.');
 
-    // Password Check.
-    $.validator.addMethod("validChars", function(value, element) {
+    // Password characters.
+    $.validator.addMethod('validChars', function(value, element) {
         // Allows `~!@#$%^&*()-_+=? symbols
         return this.optional(element) ||
             /^[A-Za-z0-9\_\.!@#\$%\^&\*\(\)~\-=\+`\?]+$/i.test(value);
-    }, "Passwords may only contain letters, numbers, and certain symbols.");
+    }, 'Passwords may only contain letters, numbers, and certain symbols.');
 
     // Validates the form upon submit and POSTs if it's valid.
-    $(document).on("submit", "#registration", function() {
-        $("#registration").validate({
+    $(document).on('submit', '#registration', function() {
+        $form = $(this);
+        $form.validate({
             messages: {
-                FirstName: {required: "Please enter your first name."},
-                LastName: {required: "Please enter your last name."},
-                Username: {required: "Please enter a username."},
-                Password: {required: "Please enter a password."},
-                Password2: {
-                        required: "Please enter a password.",
-                        equalTo: "Please enter the same password again."
+                first_name: {required: 'Please enter your first name.'},
+                last_name: {required: 'Please enter your last name.'},
+                username: {required: 'Please enter a username.'},
+                password: {required: 'Please enter a password.'},
+                password2: {
+                    required: 'Please enter a password.',
+                    equalTo: 'Please enter the same password again.'
                 },
-                SchoolName: {required: "Please enter the name of your school."},
-                SchoolAddress: {required: "Please enter the school's address."},
-                SchoolCity: {required: "Please enter the city."},
-                SchoolState: {required: "Please enter the state."},
-                SchoolZip: {required: "Please enter the zip code."},
-                SchoolCountry: {required: "Please enter the country."},
-                PrimaryName: {required: "Please enter a name."},
-                PrimaryEmail: {required: "Please enter an email address."},
-                PrimaryPhone: {required: "Please enter a phone number."}
+                school_name: {required: 'Please enter the school name.'},
+                school_address: {required: 'Please enter the school address.'},
+                school_city: {required: 'Please enter the city.'},
+                school_state: {required: 'Please enter the state.'},
+                school_zip: {required: 'Please enter the zip code.'},
+                school_country: {required: 'Please enter the country.'},
+                primary_name: {required: 'Please enter a name.'},
+                primary_email: {required: 'Please enter an email address.'},
+                primary_phone: {required: 'Please enter a phone number.'}
             },
-
             rules: {
-                Username: {
+                username: {
                     minlength:4
                 },
-                Password: {
+                password: {
                     minlength:6
                 },
-                Password2: {
-                    equalTo:"#id_Password"
+                password2: {
+                    equalTo:'#id_password'
                 },
                 program_type: {
                     minlength:1
@@ -120,13 +119,11 @@ $(function() {
             }
         });
         
-        var uri = $(this).attr("action");
-        if($(this).valid()){
-            ContentManager.loadNewContent(
-                uri,
-                $("#registration").serializeArray()
-            );
+        var uri = $(this).attr('action');
+        if($form.valid()){
+            ContentManager.loadNewContent(uri, $form.serializeArray());
         }
+
         $('html, body').animate({scrollTop:0}, 'slow');
         return false;
     });
