bd9b0caf168b3baf4e76dfddb3ed7a0f2d3c9d6d
Kunal Mehta <k.mehta@berkeley.edu>
2013-09-08 02:07:41 -0700

    #88: Killed the splash and simplified ContentManager.

--- huxley/core/static/js/core/content-manager.js
+++ huxley/core/static/js/core/content-manager.js
@@ -1,26 +1,15 @@
 var ContentManager = {
-
-    // Initializer function.
     init: function() {
         $(document).on('click', 'a.js-nav', function() {
             History.pushState({}, $(this).text(), $(this).attr('href'));
             return false;
         });
-        
-        $(document).on('click', '#appnavbar a.js-nav', function() {
-            $('#appnavbar a.js-nav.currentpage').removeClass('currentpage');
-            $(this).addClass('currentpage');
-        });
-
-        $(window).on('huxley.contentReady', ContentManager.onContentReady);
     
-        History.Adapter.bind(window, 'statechange', function(){
-            var state = History.getState();
-            var handler = $('#app').is(':hidden')
-                ? ContentManager.loadInitialContent
-                : ContentManager.loadNewContent;
-            handler(state.url);
-        });
+        History.Adapter.bind(
+            window,
+            'statechange',
+            ContentManager.onStateChange
+        );
 
         // Trigger initial app state.
         window.location.pathname == '/'
@@ -28,62 +17,62 @@ var ContentManager = {
             : History.Adapter.trigger(window, 'statechange');
     },
 
-    // Fades out the splash and fades in the app.
-    onContentReady: function() {
-        $.Deferred(function(deferred) {
-            $splash = $('#splash');
-            if ($splash.is(':visible')) {
-                $splash.delay(250).fadeOut(250, function() {
-                    deferred.resolve();
-                });
-            } else {
-                deferred.resolve();
-            }
-        }).then(function() {
-            $('#app').delay(250).fadeIn(500, function() {
+    // Handles content manipulation on state change.
+    onStateChange: function() {
+        var state = History.getState();
+        var $app = $('#app');
+        var initialLoad = $app.is(':hidden');
+
+        $('#appnavbar a.js-nav.currentpage').removeClass('currentpage');
+        $('#appnavbar a[href="' + window.location.pathname + '"]')
+            .addClass('currentpage');
+
+        if (initialLoad) {
+            $app.delay(100).fadeIn(500, function() {
                 $('#headerwrapper').slideDown(350, function() {
                     $('#header').slideDown(350);
                 });
             });
-        });
-    },
-    
-    // Loads initial content into the application content container.
-    loadInitialContent: function(path) {
-        if(document.getElementById('content-placeholder')) {
-            $.get(path, function(data) {
-                $('title').html(data.match(/<title>(.*?)<\/title>/)[1]);
-                $('#capsule').replaceWith($('#capsule', $(data)));
-                $('#appnavbar a[href="' + window.location.pathname + '"]')
-                      .addClass('currentpage');
-                $(window).trigger('huxley.contentReady');
-            });
-        } else {
-            $(window).trigger('huxley.contentReady');
+            
+            // Don't load new content if there's already content present.
+            if ($('#capsule').is(':parent')) {
+                return;
+            }
         }
+
+        ContentManager.loadNewContent(state.url, null, initialLoad);
     },
-    
+
     // Loads new content into the application content container.
-    loadNewContent: function(path, data) {
+    loadNewContent: function(path, data, skipFadeout) {
         var $content = $('.content');
         var $contentwrapper = $('#contentwrapper');
-        
-        var fadeout = $.Deferred(function(deferred) {
+
+        var fadeoutPromise = $.Deferred(function(deferred) {
             $content.css('height', $content.height() + 'px');
-            $contentwrapper.fadeOut(150, function() {
+            if (skipFadeout) {
+                $contentwrapper.hide();
                 $content.addClass('loading');
                 deferred.resolve();
-            });
-        });
+            } else {
+                $contentwrapper.fadeOut(150, function() {
+                    $content.addClass('loading');
+                    deferred.resolve();
+                });
+            }
+        }).promise();
 
-        var method = data ? 'POST' : 'GET';
-        var ajaxCall = $.ajax({type: method, url: path, data: data});
+        var fetchPromise = $.ajax({
+            type: data ? 'POST' : 'GET',
+            url: path,
+            data: data
+        }).promise();
 
-        $.when(ajaxCall, fadeout)
-            .done(function(ajaxData) {
-                var data = ajaxData[0];
-                $('title').html(data.match(/<title>(.*?)<\/title>/)[1]);
-                $('#capsule').replaceWith($('#capsule', $(data)));
+        $.when(fetchPromise, fadeoutPromise)
+            .done(function(responseData) {
+                var markup = responseData[0];
+                $('title').html(markup.match(/<title>(.*?)<\/title>/)[1]);
+                $('#capsule').replaceWith($('#capsule', $(markup)));
 
                 $contentwrapper.css({
                     'visibility':'hidden',
