db86d1d4d45b8b3c97dfc25ece41d2377c99cddc
Vishal Kalyanasundaram <vishalkal@berkeley.edu>
2013-09-12 20:07:19 -0700

    setup script revisions

--- assignment_db.py
+++ assignment_db.py
@@ -3,22 +3,22 @@ import os
 os.environ['PYTHONPATH'] = '~/Users/Vishal/huxley'
 os.environ['DJANGO_SETTINGS_MODULE'] = 'huxley.settings'
 
-from huxley import settings
 from huxley.core.models import Country, Committee, Assignment
 from xlrd import open_workbook
 
-wb = open_workbook('Country Matrix.xlsx')
+s = open_workbook('Country Matrix.xlsx').sheet_by_index(0)
 
-s = wb.sheet_by_index(0)
+country_range = s.nrows-2
+committee_range = 21
 
-for row in range(2, s.nrows-2):
+for row in range(2, country_range):
 	Country.objects.get_or_create(name=s.cell(row, 0).value, special=(True if row > 204 else False))
 
-for col in range(1, 21):
+for col in range(1, committee_range):
 	Committee.objects.get_or_create(name=s.cell(1, col).value, delegation_size=(1 if s.cell(0, col).value == 'SINGLE' else 2), special=(True if col > 15 else False))
 
-for row in range(2, s.nrows-2):
-	for col in range(1, 21):
+for row in range(2, country_range):
+	for col in range(1, committee_range):
 		if s.cell(row, col).value:
 			print s.cell(1, col).value
 			print s.cell(row, 0).value
@@ -26,7 +26,7 @@ for row in range(2, s.nrows-2):
 			print
 			country_name = Country.objects.get(name=s.cell(row, 0).value)
 			committee_name = Committee.objects.get(name=s.cell(1, col).value)
-			assignment_name = Assignment(committee=committee_name, country=country_name)
-			assignment_name.save()
+			assignment = Assignment(committee=committee_name, country=country_name)
+			assignment.save()
 
 
