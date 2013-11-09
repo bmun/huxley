e5dc52b70358bc76d68abaeef870ac276ec06c01
Kunal Mehta <k.mehta@berkeley.edu>
2013-11-09 15:49:00 -0800

    Style fixes in auth.js.

--- huxley/accounts/static/js/accounts/auth.js
+++ huxley/accounts/static/js/accounts/auth.js
@@ -1,24 +1,24 @@
 $(function() {
 
-    // Attempts to log in, and changes the UI accordingly.
-    $(document).on("submit", "form#login", function() {
+    // Attempt to log in, and change the UI accordingly.
+    $(document).on('submit', 'form#login', function() {
         var credentials = $(this).serializeArray();
-        var uri = $(this).attr("action");
-        var loginButton = $(this).find('.login-button');
+        var uri = $(this).attr('action');
+        var $loginButton = $(this).find('.login-button');
 
-        loginButton.addClass('loading');
+        $loginButton.addClass('loading');
         $.post(uri, credentials, function(data) {
-            if(data.success === true) {
+            if (data.success === true) {
                 ContentManager.onLoginLogout(data.redirect);
             } else {
-                loginButton.removeClass('loading');
-                $("#errorcontainer")
+                $loginButton.removeClass('loading');
+                $('#errorcontainer')
                     .hide()
                     .html('<label class="error">' + data.error + '</label>')
                     .fadeIn(250);
-                $("#app").effect(
-                    "shake",
-                    {direction:"up", times:2, distance:2},
+                $('#app').effect(
+                    'shake',
+                    {direction: 'up', times: 2, distance: 2},
                     250
                 );
             }
@@ -27,12 +27,12 @@ $(function() {
         return false;
     });
 
-    // Logs out and changes the UI accordingly.
-    $(document).on("click", "#logout", function() {
-        var uri = $(this).attr("href");
+    // Log out and change the UI accordingly.
+    $(document).on('click', '#logout', function() {
+        var uri = $(this).attr('href');
         $.get(uri, function(redirect) {
-            $("#header").slideUp(250, function() {
-                $("#headerwrapper").slideUp(250, function() {
+            $('#header').slideUp(250, function() {
+                $('#headerwrapper').slideUp(250, function() {
                     ContentManager.onLoginLogout(redirect);
                 });
             });
