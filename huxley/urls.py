dbe95e471b756233e6cf7968477963493f89bf9a
Kunal Mehta <k.mehta@berkeley.edu>
2013-11-09 12:56:19 -0800

    Move accounts URLs into accounts.urls.

--- huxley/urls.py
+++ huxley/urls.py
@@ -7,17 +7,8 @@ from django.views.generic import RedirectView, TemplateView
 
 admin.autodiscover()
 
-urlpatterns = patterns('huxley.accounts.views',
-    url(r'^password/reset', 'reset_password', name='reset_password'),
-    url(r'^password/change', 'change_password', name='change_password'),
-    url(r'^register', 'register', name='register'),
-    url(r'^logout', 'logout_user', name='logout'),
-    url(r'^login/user/(?P<uid>\d+)$', 'login_as_user', name='login_as_user'),
-    url(r'^login', 'login_user', name='login'),
-    url(r'^uniqueuser/', 'validate_unique_user', name='unique_user'),
-)
-
-urlpatterns += patterns('',
+urlpatterns = patterns('',
+    url(r'^', include('huxley.accounts.urls', app_name='accounts')),
     url(r'^chair/', include('huxley.chairs.urls', app_name='chairs', namespace='chairs')),
     url(r'^advisor/', include('huxley.advisors.urls', app_name='advisors', namespace='advisors')),
     url(r'^admin/', include(admin.site.urls)),
