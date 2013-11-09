957c2724f00f9c6c6634283847efddc93864e942
Kunal Mehta <k.mehta@berkeley.edu>
2013-11-09 11:02:54 -0800

    Fix whitespace in accounts.constants.

--- huxley/accounts/constants.py
+++ huxley/accounts/constants.py
@@ -2,13 +2,13 @@
 # Use of this source code is governed by a BSD License found in README.md.
 
 class ChangePasswordErrors:
-	MISSING_FIELDS = 'One or more fields is blank.'
-	MISMATCHED_PASSWORDS = 'New passwords must match.'
-	PASSWORD_TOO_SHORT = 'New password must be at least 6 characters long.'
-	INVALID_CHARACTERS = 'New password can only consist of alphanumeric characters and symbols (above numbers).'
-	INCORRECT_PASSWORD = 'Incorrect password.'
+    MISSING_FIELDS = 'One or more fields is blank.'
+    MISMATCHED_PASSWORDS = 'New passwords must match.'
+    PASSWORD_TOO_SHORT = 'New password must be at least 6 characters long.'
+    INVALID_CHARACTERS = 'New password can only consist of alphanumeric characters and symbols (above numbers).'
+    INCORRECT_PASSWORD = 'Incorrect password.'
 
 class AuthenticationErrors:
-	MISSING_FIELDS = "Whoops! One or more of the fields is blank."
-	INVALID_LOGIN = "Sorry! The login you provided was invalid."
-	INACTIVE_ACCOUNT = "We're sorry, but your account is inactive."
+    MISSING_FIELDS = "Whoops! One or more of the fields is blank."
+    INVALID_LOGIN = "Sorry! The login you provided was invalid."
+    INACTIVE_ACCOUNT = "We're sorry, but your account is inactive."
