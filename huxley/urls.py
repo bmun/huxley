089e9f7671c1e9f2dd8810de02d0d71f9a4015fe
Kunal Mehta <k.mehta@berkeley.edu>
2013-11-09 12:30:07 -0800

    Rename thanks.html to registration-success.html.

--- huxley/urls.py
+++ huxley/urls.py
@@ -26,7 +26,7 @@ urlpatterns += patterns('',
 urlpatterns += patterns('',
     url(r'^favicon\.ico$', RedirectView.as_view(url='/static/img/favicon.ico')),
     url(r'^about', TemplateView.as_view(template_name='about.html'), name='about'),
-    url(r'^success', TemplateView.as_view(template_name='thanks.html'), name='register_success'),
+    url(r'^success', TemplateView.as_view(template_name='registration-success.html'), name='register_success'),
 )
 
 urlpatterns += patterns('',
