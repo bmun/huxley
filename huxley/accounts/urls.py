e7c62c705c862909732832e8892d467e958cc1c1
Kunal Mehta <k.mehta@berkeley.edu>
2014-02-12 19:34:09 -0800

    Add optional / and enforce end-of-line to URLs
    Previously, we didn't enforce the end-of-line in our URL regexps.
    We did this so that URLs wouldn't have the ugly backslash character
    when looked up from code. But, this meant that a user could go
    to /loginlololol and it would match the login URL.
    
    Now, we match the end-of-line, but add an optional slash to support
    those (admittedly valid) URLs. The good news is that the reverse
    lookup omits them!

--- huxley/accounts/urls.py
+++ huxley/accounts/urls.py
@@ -4,11 +4,11 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('huxley.accounts.views',
-    url(r'^password/reset', 'reset_password', name='reset_password'),
-    url(r'^password/change', 'change_password', name='change_password'),
-    url(r'^register', 'register', name='register'),
-    url(r'^logout', 'logout_user', name='logout'),
-    url(r'^login/user/(?P<uid>\d+)$', 'login_as_user', name='login_as_user'),
-    url(r'^login/$', 'login_user', name='login'),
-    url(r'^uniqueuser/', 'validate_unique_user', name='unique_user'),
+    url(r'^password/reset/?$', 'reset_password', name='reset_password'),
+    url(r'^password/change/?$', 'change_password', name='change_password'),
+    url(r'^register/?$', 'register', name='register'),
+    url(r'^logout/?$', 'logout_user', name='logout'),
+    url(r'^login/user/(?P<uid>\d+)/?$', 'login_as_user', name='login_as_user'),
+    url(r'^login/?$', 'login_user', name='login'),
+    url(r'^uniqueuser/?$', 'validate_unique_user', name='unique_user'),
 )
