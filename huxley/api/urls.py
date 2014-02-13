9e47ca6ddeeb699b17d31cb932f8ce1c878e8e03
Kunal Mehta <k.mehta@berkeley.edu>
2014-02-12 19:51:43 -0800

    Name the URLs in API

--- huxley/api/urls.py
+++ huxley/api/urls.py
@@ -7,8 +7,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from huxley.api import views
 
 urlpatterns = patterns('',
-    url(r'^users/?$', views.UserList.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)/?$', views.UserDetail.as_view()),
+    url(r'^users/?$', views.UserList.as_view(), name='user_list'),
+    url(r'^users/(?P<pk>[0-9]+)/?$', views.UserDetail.as_view(), name='user_detail'),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
