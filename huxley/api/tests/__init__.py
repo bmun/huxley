303c444cba5ca28fac52e01a2e10e38a13950d81
Kunal Mehta <k.mehta@berkeley.edu>
2014-04-19 17:06:57 -0700

    Add TestSchools class and a sanity check SchoolDetailTestCase.

--- huxley/api/tests/__init__.py
+++ huxley/api/tests/__init__.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2011-2014 Berkeley Model United Nations. All rights reserved.
 # Use of this source code is governed by a BSD License (see LICENSE).
 
+from school import *
 from user import *
--- huxley/utils/test.py
+++ huxley/utils/test.py
@@ -2,6 +2,7 @@
 # Use of this source code is governed by a BSD License (see LICENSE).
 
 from huxley.accounts.models import HuxleyUser
+from huxley.core.models import School
 
 class TestUsers():
     @staticmethod
@@ -25,3 +26,36 @@ class TestUsers():
     def new_superuser(*args, **kwargs):
         kwargs['is_superuser'] = True
         return TestUsers.new_user(**kwargs)
+
+class TestSchools():
+    @staticmethod
+    def new_school(**kwargs):
+        s = School(name=kwargs.get('name', 'Test School'),
+                   address=kwargs.get('address', '1 Schoolhouse Road'),
+                   city=kwargs.get('city', 'Berkeley'),
+                   state=kwargs.get('state', 'CA'),
+                   zip_code=kwargs.get('zip_code', '94024'),
+                   country=kwargs.get('country', 'United States of America'),
+                   primary_name=kwargs.get('primary_name', ''),
+                   primary_email=kwargs.get('primary_email', ''),
+                   primary_phone=kwargs.get('primary_phone', ''),
+                   secondary_name=kwargs.get('secondary_name', ''),
+                   secondary_email=kwargs.get('secondary_email', ''),
+                   secondary_phone=kwargs.get('secondary_phone', ''),
+                   program_type=kwargs.get('program_type', School.TYPE_CLUB),
+                   times_attended=kwargs.get('times_attended', 0),
+                   min_delegation_size=kwargs.get('min_delegation_size', 0),
+                   max_delegation_size=kwargs.get('max_delegation_size', 0))
+
+        skip = {'name', 'address', 'city', 'state', 'zip_code', 'country',
+                'primary_name', 'primary_email', 'primary_phone',
+                'secondary_name', 'secondary_email', 'secondary_phone',
+                'program_type', 'times_attended', 'min_delegation_size',
+                'max_delegation_size'}
+        for attr, value in kwargs.items():
+            if attr not in skip:
+                setattr(s, attr, value)
+
+        s.save()
+        TestUsers.new_user(school=s)
+        return s
