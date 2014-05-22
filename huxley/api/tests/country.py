3c26d95f5ca0a6d14ff51a31b6d7aa49ce5a46a9
Trevor Dowds <tdowds@berkeley.edu>
2014-05-21 18:25:59 -0700

    #140 whitespace change

--- huxley/api/tests/country.py
+++ huxley/api/tests/country.py
@@ -21,37 +21,37 @@ class CountryDetailGetTestCase(TestCase):
     def get_response(self, url):
         return json.loads(self.client.get(url).content)
 
-	def test_anonymous_user(self):
-		'''Fields should be returned when accessed by any user.'''
-		country = TestCountries.new_country()
-		url = self.get_url(country.id)
-		response = self.get_response(url)
-		self.assertEqual(response['name'], country.name)
-		self.assertEqual(response['special'], country.special)
+    def test_anonymous_user(self):
+        '''Fields should be returned when accessed by any user.'''
+        country = TestCountries.new_country()
+        url = self.get_url(country.id)
+        response = self.get_response(url)
+        self.assertEqual(response['name'], country.name)
+        self.assertEqual(response['special'], country.special)
 
 class CountryListGetTestCase(TestCase):
-	def setUp(self):
-		self.client = Client()
-		self.url = reverse('api:country_list')
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('api:country_list')
 
-	def get_data(self):
-		return json.loads(self.client.get(self.url).content)
+    def get_data(self):
+        return json.loads(self.client.get(self.url).content)
 
-	def test_anonymous_user(self):
-		'''Anyone should be able to access a list of all the countries.'''
-		country1 = TestCountries.new_country(name='USA')
-		country2 = TestCountries.new_country(name='China')
-		country3 = TestCountries.new_country(name='Barbara Boxer', special=True)
-		data = self.get_data()
-		self.assertEqual(data[0],
-						 {'id': country1.id,
-						  'special': country1.special,
-						  'name': country1.name})
-		self.assertEqual(data[1],
-						 {'id': country2.id,
-						  'special': country2.special,
-						  'name': country2.name})
-		self.assertEqual(data[2],
-						 {'id': country3.id,
-						  'special': country3.special,
-						  'name': country3.name})
+    def test_anonymous_user(self):
+        '''Anyone should be able to access a list of all the countries.'''
+        country1 = TestCountries.new_country(name='USA')
+        country2 = TestCountries.new_country(name='China')
+        country3 = TestCountries.new_country(name='Barbara Boxer', special=True)
+        data = self.get_data()
+        self.assertEqual(data[0],
+                         {'id': country1.id,
+                          'special': country1.special,
+                          'name': country1.name})
+        self.assertEqual(data[1],
+                         {'id': country2.id,
+                          'special': country2.special,
+                          'name': country2.name})
+        self.assertEqual(data[2],
+                         {'id': country3.id,
+                          'special': country3.special,
+                          'name': country3.name})
