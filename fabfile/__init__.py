49ffa2f8230ea261ab97debcc3a8243c66651023
Trevor Dowds <tdowds@berkeley.edu>
2014-05-23 15:17:17 -0700

    #149 Updated the test runner for Django 1.6

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -40,7 +40,7 @@ def update():
 def test(*args):
     '''Run the project's test suite, with optionally specified apps.'''
     with hide('aborts', 'warnings'):
-        return local('python manage.py test %s' % ' '.join(args))
+        return local('python manage.py test ./huxley/%s' % ' '.join(args))
 
 
 @task
