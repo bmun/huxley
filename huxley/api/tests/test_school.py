1bc0e22b10de206dbdca17de976fa0a2e823d478
Nathaniel Parke <nathaniel.parke@gmail.com>
2014-05-27 16:29:08 -0700

    basic tests

--- huxley/api/tests/test_school.py
+++ huxley/api/tests/test_school.py
@@ -7,25 +7,44 @@ from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.client import Client
 
-from huxley.utils.test import TestSchools
+from huxley.utils.test import TestSchools, TestUsers
+from huxley.api.tests import GetAPITestCase
 
 
-class SchoolDetailTestCase(TestCase):
-    def setUp(self):
-        self.client = Client()
-
-    def get_url(self, school_id):
-        return reverse('api:school_detail', args=(school_id,))
-
-    def get_response(self, url):
-        return json.loads(self.client.get(url).content)
+class SchoolDetailGetTestCase(GetAPITestCase):
+    url_name = 'api:school_detail'
 
     def test_anonymous_user(self):
         '''It should reject request from an anonymous user.'''
         school = TestSchools.new_school()
-        url = self.get_url(school.id)
-        data = self.get_response(url)
+        data = self.get_response(school.id)
 
         self.assertEqual(len(data.keys()), 1)
         self.assertEqual(data['detail'],
                          u'Authentication credentials were not provided.')
+
+    def test_self(self):
+        '''It should allow the get request from the user.'''
+        school = TestSchools.new_school()
+        user = TestUsers.objects.get(school=school)
+        data = self.get_response(school.id)
+
+        self.client.login(username='user', password='user')
+
+
+
+class SchoolDetailPutTestCase(TestCase):
+    pass
+
+class SchoolDetailPatchTestCase(TestCase):
+    pass
+
+class SchoolDetailDeleteTestCase(TestCase):
+    pass
+
+class SchoolListGetTestCase(TestCase):
+    pass
+
+class SchoolListPostTestCase(TestCase):
+    pass
+
