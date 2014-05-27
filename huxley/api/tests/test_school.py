a6680ce7db914c34279b39c55f7d98b880de1276
Nathaniel Parke <nathaniel.parke@gmail.com>
2014-05-27 16:29:08 -0700

    (#141) Unit test School API.
    Closes #170.

--- huxley/api/tests/test_school.py
+++ huxley/api/tests/test_school.py
@@ -7,25 +7,290 @@ from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.client import Client
 
-from huxley.utils.test import TestSchools
+from huxley.core.models import School
+from huxley.utils.test import TestSchools, TestUsers
+from huxley.api.tests import (CreateAPITestCase, DestroyAPITestCase,
+                              ListAPITestCase, PartialUpdateAPITestCase,
+                              RetrieveAPITestCase)
+from huxley.accounts.models import HuxleyUser
 
 
-class SchoolDetailTestCase(TestCase):
-    def setUp(self):
-        self.client = Client()
-
-    def get_url(self, school_id):
-        return reverse('api:school_detail', args=(school_id,))
-
-    def get_response(self, url):
-        return json.loads(self.client.get(url).content)
+class SchoolDetailGetTestCase(RetrieveAPITestCase):
+    url_name = 'api:school_detail'
 
     def test_anonymous_user(self):
         '''It should reject request from an anonymous user.'''
         school = TestSchools.new_school()
-        url = self.get_url(school.id)
-        data = self.get_response(url)
+        response = self.get_response(school.id)
 
-        self.assertEqual(len(data.keys()), 1)
-        self.assertEqual(data['detail'],
-                         u'Authentication credentials were not provided.')
+        self.assertEqual(response.data, {
+            'detail': u'Authentication credentials were not provided.'})
+
+    def test_self(self):
+        '''It should allow the get request from the user.'''
+        school = TestSchools.new_school()
+        user = school.advisor
+
+        self.client.login(username='testuser', password='test')
+        response = self.get_response(school.id)
+
+        self.assertEqual(response.data, {
+            'id': school.id,
+            'registered': school.registered,
+            'name': school.name,
+            'address': school.address,
+            'city': school.city,
+            'state': school.state,
+            'zip_code': school.zip_code,
+            'country': school.country,
+            'primary_name': school.primary_name,
+            'primary_email': school.primary_email,
+            'primary_phone': school.primary_phone,
+            'secondary_name': school.secondary_name,
+            'secondary_email': school.secondary_email,
+            'secondary_phone': school.secondary_phone,
+            'program_type': school.program_type,
+            'times_attended': school.times_attended,
+            'min_delegation_size': school.min_delegation_size,
+            'max_delegation_size': school.max_delegation_size,
+            'international': school.international,
+            'waitlist': school.waitlist})
+
+    def test_other_user(self):
+        '''it should not allow a get request from another user.'''
+        school = TestSchools.new_school()
+        user1 = school.advisor
+        user2 = TestUsers.new_user(username='user2', password='user2')
+
+        self.client.login(username='user2', password='user2')
+        response = self.get_response(school.id)
+
+        self.assertEqual(response.data, {
+            'detail': u'You do not have permission to perform this action.'})
+
+    def test_superuser(self):
+        '''it should allow a get request from a superuser.'''
+        school = TestSchools.new_school()
+        user = TestUsers.new_superuser(username='user1', password='user1')
+
+        self.client.login(username='user1', password='user1')
+        response = self.get_response(school.id)
+
+        self.assertEqual(response.data, {
+            'id': school.id,
+            'registered': school.registered,
+            'name': school.name,
+            'address': school.address,
+            'city': school.city,
+            'state': school.state,
+            'zip_code': school.zip_code,
+            'country': school.country,
+            'primary_name': school.primary_name,
+            'primary_email': school.primary_email,
+            'primary_phone': school.primary_phone,
+            'secondary_name': school.secondary_name,
+            'secondary_email': school.secondary_email,
+            'secondary_phone': school.secondary_phone,
+            'program_type': school.program_type,
+            'times_attended': school.times_attended,
+            'min_delegation_size': school.min_delegation_size,
+            'max_delegation_size': school.max_delegation_size,
+            'international': school.international,
+            'waitlist': school.waitlist})
+
+
+class SchoolDetailPatchTestCase(PartialUpdateAPITestCase):
+    url_name = 'api:school_detail'
+    params = {'name': 'school_name', 'city': 'school_city'}
+
+    def setUp(self):
+        self.school = TestSchools.new_school()
+        self.user = self.school.advisor
+
+    def test_anonymous_user(self):
+        '''Should not be able to update with an anonymous user.'''
+        response = self.get_response(self.school.id)
+        updated_school = School.objects.get(id=self.school.id)
+
+        self.assertEqual(response.data, {
+            'detail': u'Authentication credentials were not provided.'})
+        self.assertEqual(updated_school.name, self.school.name)
+        self.assertEqual(updated_school.city, self.school.city)
+
+    def test_self(self):
+        '''You should be able to update with an anonymous user.'''
+        self.client.login(username='testuser', password='test')
+        response = self.get_response(self.school.id, params=self.params)
+        self.school = School.objects.get(id=self.school.id)
+
+        self.assertEqual(response.data['name'], self.school.name)
+        self.assertEqual(response.data['city'], self.school.city)
+
+    def test_other_user(self):
+        '''Should not allow another user to change a school's data'''
+        user2 = TestUsers.new_user(username='user2', password='user2')
+        self.client.login(username='user2', password='user2')
+        response = self.get_response(self.school.id, params=self.params)
+        updated_school = School.objects.get(id=self.school.id)
+
+        self.assertEqual(response.data, {
+            u'detail': u'You do not have permission to perform this action.'})
+        self.assertEqual(updated_school.name, self.school.name)
+        self.assertEqual(updated_school.city, self.school.city)
+
+    def test_superuser(self):
+        '''This this allow  a superuser to change school data.'''
+        user2 = TestUsers.new_superuser(username='user2', password='user2')
+        self.client.login(username='user2', password='user2')
+        response = self.get_response(self.school.id, params=self.params)
+        self.school = School.objects.get(id=self.school.id)
+
+        self.assertEqual(response.data['name'], self.school.name)
+        self.assertEqual(response.data['city'], self.school.city)
+
+
+class SchoolDetailDeleteTestCase(DestroyAPITestCase):
+    url_name = 'api:school_detail'
+
+    def setUp(self):
+        self.school = TestSchools.new_school()
+        self.user = self.school.advisor
+
+    def test_anonymous_user(self):
+        '''Should not be able to delete anonymously.'''
+        response = self.get_response(self.school.id)
+
+        self.assertEqual(response.data, {
+            'detail': u'Authentication credentials were not provided.'})
+        self.assertTrue(School.objects.filter(id=self.school.id).exists())
+
+    def test_self(self):
+        '''One user should be able to delete their own account.'''
+        self.client.login(username='testuser', password='test')
+        response = self.get_response(self.school.id)
+
+        self.assertEqual(response.data, None)
+        self.assertFalse(School.objects.filter(id=self.school.id).exists())
+
+    def test_other_user(self):
+        '''One user should not be able to delete another user.'''
+        user2 = TestUsers.new_user(username='user2', password='user2')
+        self.client.login(username='user2', password='user2')
+        response = self.get_response(self.school.id)
+
+        self.assertEqual(response.data, {
+            u'detail': u'You do not have permission to perform this action.'})
+        self.assertTrue(School.objects.filter(id=self.school.id).exists())
+
+    def test_superuser(self):
+        '''A superuser should not be able to delete an account.'''
+        user2 = TestUsers.new_user(username='user2', password='user2')
+        self.client.login(username='user2', password='user2')
+        response = self.get_response(self.school.id)
+
+        self.assertEqual(response.data, {
+            u'detail': u'You do not have permission to perform this action.'})
+        self.assertTrue(School.objects.filter(id=self.school.id).exists())
+
+
+class SchoolListGetTestCase(ListAPITestCase):
+    url_name = 'api:school_list'
+
+    def setUp(self):
+        self.school = TestSchools.new_school()
+        self.user = self.school.advisor
+
+    def test_anonymous_user(self):
+        '''It should reject an anonymous user.'''
+        response = self.get_response()
+
+        self.assertEqual(response.data, {
+            'detail': u'Authentication credentials were not provided.'})
+
+    def test_self(self):
+        '''It should reject a request from an unauthorized user.'''
+        self.client.login(username='testuser', password='test')
+        response = self.get_response()
+
+        self.assertEqual(response.data, {
+            'detail': u'You do not have permission to perform this action.'})
+
+    def test_superuser(self):
+        '''It should reject a request from a superuser.'''
+        user = TestUsers.new_superuser(username='user', password='user')
+
+        self.client.login(username='user', password='user')
+        response = self.get_response()
+
+        self.assertEqual(response.data, {
+            'detail': u"Method 'GET' not allowed."})
+
+
+class SchoolListPostTestCase(CreateAPITestCase):
+    url_name = 'api:school_list'
+    params = {'name': 'Berkeley Prep',
+            'address': '1 BMUN way',
+            'city': 'Oakland',
+            'state': 'California',
+            'zip_code': 94720,
+            'country': 'USA',
+            'primary_name': 'Kunal Mehta',
+            'primary_email': 'KunalMehta@huxley.org',
+            'primary_phone': '9999999999',
+            'program_type': HuxleyUser.TYPE_ADVISOR}
+
+    def test_empty_fields(self):
+        '''This should not allow for required fields to be empty.'''
+        response = self.get_response(params=self.get_params(name='',
+                                                            address='',
+                                                            city='',
+                                                            state='',
+                                                            zip_code='',
+                                                            country='',
+                                                            primary_name='',
+                                                            primary_email='',
+                                                            primary_phone='',
+                                                            program_type=''))
+        self.assertEqual(response.data,
+            {"city": ["This field is required."],
+            "name": ["This field is required."],
+            "primary_phone": ["This field is required."],
+            "program_type": ["This field is required."],
+            "country": ["This field is required."],
+            "state": ["This field is required."],
+            "primary_name": ["This field is required."],
+            "primary_email": ["This field is required."],
+            "address": ["This field is required."],
+            "zip_code": ["This field is required."]})
+
+    def test_valid(self):
+        params = self.get_params()
+        response = self.get_response(params)
+
+        school_query = School.objects.filter(id=response.data['id'])
+        self.assertTrue(school_query.exists())
+
+        school = School.objects.get(id=response.data['id'])
+        self.assertEqual(response.data, {
+            'id': school.id,
+            'registered': school.registered,
+            'name': school.name,
+            'address': school.address,
+            'city': school.city,
+            'state': school.state,
+            'zip_code': school.zip_code,
+            'country': school.country,
+            'primary_name': school.primary_name,
+            'primary_email': school.primary_email,
+            'primary_phone': school.primary_phone,
+            'secondary_name': school.secondary_name,
+            'secondary_email': school.secondary_email,
+            'secondary_phone': school.secondary_phone,
+            'program_type': school.program_type,
+            'times_attended': school.times_attended,
+            'min_delegation_size': school.min_delegation_size,
+            'max_delegation_size': school.max_delegation_size,
+            'international': school.international,
+            'waitlist': school.waitlist})
+
+
