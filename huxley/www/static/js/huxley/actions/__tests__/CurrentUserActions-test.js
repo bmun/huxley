0196b82e0282950b30796421ce296ac26f8602f0
Kunal Mehta <k.mehta@berkeley.edu>
2014-05-27 21:31:39 -0700

    Add Jest and CurrentUserActions-test.js.
    This adds the first JS unit test as an example. To keep it small,
    I tested the most basic component, so it could demonstrate the
    fundamental parts of a Jest test: not mocking the module under test,
    'describe', 'beforeEach', 'it', and 'expect'.

---
+++ huxley/www/static/js/huxley/actions/__tests__/CurrentUserActions-test.js
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2011-2014 Berkeley Model United Nations. All rights reserved.
+ * Use of this source code is governed by a BSD License (see LICENSE).
+ */
+
+'use strict';
+
+jest.dontMock('../CurrentUserActions');
+
+describe('CurrentUserActions', function() {
+  var ActionConstants;
+  var CurrentUserActions;
+  var Dispatcher;
+
+  beforeEach(function() {
+    ActionConstants = require('../../constants/ActionConstants');
+    CurrentUserActions = require('../CurrentUserActions');
+    Dispatcher = require('../../dispatcher/Dispatcher');
+  });
+
+  it('dispatches the login action', function() {
+    var user = {id: 1, first_name: 'Mike', last_name: 'Jones'};
+    CurrentUserActions.login(user);
+
+    expect(Dispatcher.dispatch.mock.calls.length).toBe(1);
+    expect(Dispatcher.dispatch).toBeCalledWith({
+      actionType: ActionConstants.LOGIN,
+      user: user
+    });
+  });
+
+  it('dispatches the logout action', function() {
+    CurrentUserActions.logout();
+
+    expect(Dispatcher.dispatch.mock.calls.length).toBe(1);
+    expect(Dispatcher.dispatch).toBeCalledWith({
+      actionType: ActionConstants.LOGOUT
+    });
+  });
+});
