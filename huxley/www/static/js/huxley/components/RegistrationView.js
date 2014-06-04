0171ee7a99dc25b0d625f24806c434d8fd8db390
Trevor Dowds <tdowds@berkeley.edu>
2014-06-03 20:48:05 -0700

    #160 style adjustments for committee prefs in registration view.

--- huxley/www/static/js/huxley/components/RegistrationView.js
+++ huxley/www/static/js/huxley/components/RegistrationView.js
@@ -61,26 +61,6 @@ var RegistrationView = React.createClass({
     };
   },
 
-  renderOptions: function() {
-    return this.state.countries.map(function(country) {
-      return <option key={country.id} value={country.id}>{country.name}</option>
-    });
-  },
-
-  renderCommittees: function() {
-    return this.state.committees.map(function(committee) {
-      return <li>
-                <label name="committee_prefs">
-                  <input
-                    className="choice"
-                    type="checkbox"
-                    name="committee_prefs"
-                  /> {committee.full_name}
-                </label>
-              </li>
-    });
-  },
-
   componentDidMount: function() {
     CountryStore.getCountries(function(countries) {
       this.setState({countries: countries});
@@ -419,6 +399,29 @@ var RegistrationView = React.createClass({
         </form>
       </OuterView>
     );
+  },
+
+  renderCommittees: function() {
+    return this.state.committees.map(function(committee) {
+      return (
+        <li>
+          <label name="committee_prefs">
+            <input
+              className="choice"
+              type="checkbox"
+              name="committee_prefs"
+            />
+            {committee.full_name}
+          </label>
+        </li>
+      );
+    });
+  },
+
+  renderOptions: function() {
+    return this.state.countries.map(function(country) {
+      return <option key={country.id} value={country.id}>{country.name}</option>
+    });
   },
 
   _handleSubmit: function(event) {
