d3841c8f473630093e24aff2fbd8a06976bbcb12
Kunal Mehta <k.mehta@berkeley.edu>
2014-06-12 23:07:47 -0700

    Clean up core.views.index.

--- huxley/core/views.py
+++ huxley/core/views.py
@@ -1,15 +1,12 @@
 # Copyright (c) 2011-2014 Berkeley Model United Nations. All rights reserved.
 # Use of this source code is governed by a BSD License (see LICENSE).
 
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+from huxley.utils.shortcuts import render_template
 
-from huxley.core.models import *
 
 def index(request):
     '''Render the appropriate base tempate.'''
-    context = RequestContext(request)
     if not request.user.is_authenticated():
-        return render_to_response('login.html', context)
+        return render_template(request, 'login.html')
     else:
-        return render_to_response('base-inner.html', context)
+        return render_template(request, 'base-inner.html')
