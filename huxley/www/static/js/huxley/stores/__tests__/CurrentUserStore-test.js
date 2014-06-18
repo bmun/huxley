bab1d9cfda17056e1aaac80ddc02f1f46d19cfbb
Trevor Dowds <tdowds@berkeley.edu>
2014-06-17 23:08:57 -0700

    #169 change emitter test.

--- huxley/www/static/js/huxley/stores/__tests__/CurrentUserStore-test.js
+++ huxley/www/static/js/huxley/stores/__tests__/CurrentUserStore-test.js
@@ -6,43 +6,63 @@
 'use strict';
 
 jest.dontMock('../CurrentUserStore')
-jest.dontMock('../../dispatcher/Dispatcher')
-jest.dontMock('../../actions/CurrentUserActions')
-jest.dontMock('../../User');
+jest.dontMock('../../User')
+jest.dontMock('events')
 
 describe('CurrentUserStore', function() {
+  var ActionConstants
   var CurrentUserStore;
-  var User;
   var Dispatcher;
-  var CurrentUserActions;
+  var registerCallBack;
 
   beforeEach(function() {
+    ActionConstants = require('../../constants/ActionConstants');
     CurrentUserStore = require('../CurrentUserStore');
-    User = require('../../User');
     Dispatcher = require('../../dispatcher/Dispatcher');
-    CurrentUserActions = require('../../actions/CurrentUserActions');
+
+    registerCallBack = Dispatcher.register.mock.calls[0][0];
     global.currentUser = {id: 1, user_type: 1};
     CurrentUserStore.bootstrap();
   });
 
   it('correctly bootstraps the current user object', function() {
     expect(CurrentUserStore.getCurrentUser()).not.toBeUndefined();
-  })
+  });
 
   it('deletes the reference to the global user object', function() {
     expect(global.currentUser).toBeUndefined();
   });
 
+  it('subscribes to the dispatcher', function() {
+    expect(Dispatcher.register).toBeCalled();
+  });
+
   it('sets a new user on login', function() {
     var user = CurrentUserStore.getCurrentUser();
-    CurrentUserActions.login(user.getData());
+    registerCallBack({
+      actionType: ActionConstants.LOGIN,
+      user: user
+    });
     var mockUser = CurrentUserStore.getCurrentUser();
     expect(user).not.toBe(mockUser);
   });
 
   it('sets an anonymous user on logout', function() {
-    CurrentUserActions.logout();
+    registerCallBack({
+      actionType: ActionConstants.LOGOUT,
+    });
     var mockUser = CurrentUserStore.getCurrentUser();
-    expect(mockUser.isAnonymous()).toBeTruthy();
-  })
+    expect(mockUser.isAnonymous()).toBe(true);
+  });
+
+  it('emits a change when the user changes', function() {
+    var callback = jest.genMockFunction();
+    CurrentUserStore.addChangeListener(callback);
+    expect(callback).not.toBeCalled();
+    registerCallBack({
+      actionType: ActionConstants.LOGIN,
+      user: {id: 1, user_type: 1}
+    });
+    expect(callback).toBeCalled();
+  });
 });
