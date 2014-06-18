8f7bf71d3158426b3cc286aec2d018c12f859e69
Trevor Dowds <tdowds@berkeley.edu>
2014-06-17 18:33:19 -0700

    #169 logging in and out tests.

--- huxley/www/static/js/huxley/stores/__tests__/CurrentUserStore-test.js
+++ huxley/www/static/js/huxley/stores/__tests__/CurrentUserStore-test.js
@@ -6,24 +6,43 @@
 'use strict';
 
 jest.dontMock('../CurrentUserStore')
+jest.dontMock('../../dispatcher/Dispatcher')
+jest.dontMock('../../actions/CurrentUserActions')
+jest.dontMock('../../User');
 
 describe('CurrentUserStore', function() {
   var CurrentUserStore;
   var User;
+  var Dispatcher;
+  var CurrentUserActions;
 
   beforeEach(function() {
     CurrentUserStore = require('../CurrentUserStore');
     User = require('../../User');
+    Dispatcher = require('../../dispatcher/Dispatcher');
+    CurrentUserActions = require('../../actions/CurrentUserActions');
+    global.currentUser = {id: 1, user_type: 1};
+    CurrentUserStore.bootstrap();
   });
 
+  it('correctly bootstraps the current user object', function() {
+    expect(CurrentUserStore.getCurrentUser()).not.toBeUndefined();
+  })
+
   it('deletes the reference to the global user object', function() {
-    global.currentUser = { user_type: 1 }
-    CurrentUserStore.bootstrap()
     expect(global.currentUser).toBeUndefined();
   });
 
-  it('subscribes to the dispatcher', function() {
-    var Dispatcher = require('../../dispatcher/Dispatcher');
-    expect(Dispatcher.register.mock.calls.length).toEqual(1);
+  it('sets a new user on login', function() {
+    var user = CurrentUserStore.getCurrentUser();
+    CurrentUserActions.login(user.getData());
+    var mockUser = CurrentUserStore.getCurrentUser();
+    expect(user).not.toBe(mockUser);
   });
+
+  it('sets an anonymous user on logout', function() {
+    CurrentUserActions.logout();
+    var mockUser = CurrentUserStore.getCurrentUser();
+    expect(mockUser.isAnonymous()).toBeTruthy();
+  })
 });
