7512d68000124f644ca90a7531ae42458d05afb3
Trevor Dowds <tdowds@berkeley.edu>
2014-08-08 18:08:11 -0700

    #219 made AdvisorProfileView static.

--- huxley/www/static/js/huxley/components/AdvisorProfileView.js
+++ huxley/www/static/js/huxley/components/AdvisorProfileView.js
@@ -22,28 +22,6 @@ var AdvisorProfileView = React.createClass({
     user: React.PropTypes.instanceOf(User).isRequired
   },
 
-  getInitialState: function() {
-    var user = this.props.user.getData();
-    var school = user.school;
-    return {
-      first_name: user.first_name,
-      last_name: user.last_name,
-      school_name: school.name,
-      school_address: school.address,
-      school_city: school.city,
-      school_zip_code: school.zip_code,
-      program_type: school.program_type,
-      times_attended: school.times_attended,
-      delegation_size: school.delegation_size,
-      primary_name: school.primary_name,
-      primary_email: school.primary_email,
-      primary_phone: school.primary_phone,
-      secondary_name: school.secondary_name,
-      secondary_email: school.secondary_email,
-      secondary_phone: school.secondary_phone
-    }
-  },
-
   render: function() {
     var user = this.props.user.getData();
     var school = user.school;
@@ -68,7 +46,6 @@ var AdvisorProfileView = React.createClass({
         Under-Secretary General of External Relations, 63rd Session.</p>
         <form id="welcomepage">
           <div className="tablemenu header">
-            Dont forget to save!
           </div>
           <div id="welcomeinfocontainer" className="table-container">
             <table id="welcomeinfo" className="table highlight-cells">
@@ -78,19 +55,13 @@ var AdvisorProfileView = React.createClass({
               <tr>
                 <td className="fieldLabel">First Name</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('first_name')}
-                  />
+                  {user.first_name}
                 </td>
               </tr>
               <tr>
                 <td className="fieldLabel">Last Name</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('last_name')}
-                  />
+                  {user.last_name}
                 </td>
               </tr>
               <tr>
@@ -99,37 +70,25 @@ var AdvisorProfileView = React.createClass({
               <tr>
                 <td className="fieldLabel">Name</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('school_name')}
-                  />
+                  {school.name}
                 </td>
               </tr>
               <tr>
                 <td className="fieldLabel">Address</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('school_address')}
-                  />
+                  {school.address}
                 </td>
               </tr>
               <tr>
                 <td className="fieldLabel">City</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('school_city')}
-                  />
+                  {school.city}
                 </td>
               </tr>
               <tr>
                 <td className="fieldLabel">Zip</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('school_zip_code')}
-                  />
+                  {school.zip_code}
                 </td>
               </tr>
               <tr>
@@ -138,39 +97,38 @@ var AdvisorProfileView = React.createClass({
               <tr>
                 <td className="fieldLabel">Program Type</td>
                 <td className="field">
-                  <input
-                    type="radio"
-                    name="program_type"
-                    valueLink={this.linkState('program_type')}
-                    checked={ProgramTypes.CLASS === school.program_type}
-                  />
-                  <label>Class</label>
-                  &nbsp;&nbsp;&nbsp;&nbsp;
-                  <input
-                    type="radio"
-                    name="program_type"
-                    valueLink={this.linkState('program_type')}
-                    checked={ProgramTypes.CLUB === school.program_type}
-                  />
-                  <label>Club</label>
+                  {school.program_type === 1 ? 'Club' : 'Class'}
                 </td>
               </tr>
               <tr>
                 <td className="fieldLabel">Times Attended</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('times_attended')}
-                  />
+                  {school.times_attended}
                 </td>
               </tr>
               <tr>
-                <td className="fieldLabel">Delegation Size</td>
+                <td className="fieldLabel">Number of Beginner Delegates</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('delegation_size')}
-                  />
+                  {school.beginner_delegates}
+                </td>
+              </tr>
+              <tr>
+                <td className="fieldLabel">Number of Intermediate Delegates</td>
+                <td className="field">
+                  {school.intermediate_delegates}
+                </td>
+              </tr>
+              <tr>
+                <td className="fieldLabel">Number of Advanced Delegates</td>
+                <td className="field">
+                  {school.advanced_delegates}
+                </td>
+              </tr>
+              <tr>
+                <td className="fieldLabel">Number of Spanish Speaking
+                Delegates</td>
+                <td className="field">
+                  {school.spanish_speaking_delegates}
                 </td>
               </tr>
               <tr>
@@ -179,28 +137,19 @@ var AdvisorProfileView = React.createClass({
               <tr>
                 <td className="fieldLabel">Name</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('primary_name')}
-                  />
+                  {school.primary_name}
                 </td>
               </tr>
               <tr>
                 <td className="fieldLabel">Email</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('primary_email')}
-                  />
+                  {school.primary_email}
                 </td>
               </tr>
               <tr>
                 <td className="fieldLabel">Phone</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('primary_phone')}
-                  />
+                  {school.primary_phone}
                 </td>
               </tr>
               <tr>
@@ -209,34 +158,24 @@ var AdvisorProfileView = React.createClass({
               <tr>
                 <td className="fieldLabel">Name</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('secondary_name')}
-                  />
+                  {school.secondary_name}
                 </td>
               </tr>
               <tr>
                 <td className="fieldLabel">Email</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('secondary_email')}
-                  />
+                  {school.secondary_email}
                 </td>
               </tr>
               <tr>
                 <td className="fieldLabel">Phone</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('secondary_phone')}
-                  />
+                  {school.secondary_phone}
                 </td>
               </tr>
             </table>
           </div>
           <div className="tablemenu footer">
-            Dont forget to save!
           </div>
         </form>
         <br />
