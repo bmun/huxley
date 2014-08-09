a2eccc4186e44e82ad7e373636ac8c9963adf4f9
Trevor Dowds <tdowds@berkeley.edu>
2014-08-08 18:08:11 -0700

    Make AdvisorProfileView static.
    The fields are non-editable, and they don't highlight on hover.
    Resolves #219, Closes #242.

--- huxley/www/static/js/huxley/components/AdvisorProfileView.js
+++ huxley/www/static/js/huxley/components/AdvisorProfileView.js
@@ -22,40 +22,15 @@ var AdvisorProfileView = React.createClass({
     user: React.PropTypes.instanceOf(User).isRequired
   },
 
-  getInitialState: function() {
-    var user = this.props.user.getData();
-    var school = user.school;
-    return {
-      first_name: user.first_name,
-      last_name: user.last_name,
-      school_name: school.name,
-      school_address: school.address,
-      school_city: school.city,
-      school_zip_code: school.zip_code,
-      program_type: school.program_type,
-      times_attended: school.times_attended,
-      delegation_size: school.delegation_size,
-      primary_name: school.primary_name,
-      primary_email: school.primary_email,
-      primary_phone: school.primary_phone,
-      secondary_name: school.secondary_name,
-      secondary_email: school.secondary_email,
-      secondary_phone: school.secondary_phone
-    }
-  },
-
   render: function() {
     var user = this.props.user.getData();
     var school = user.school;
     return (
       <InnerView>
         <h2>Welcome, {user.first_name}!</h2>
-        <p>We're very excited to see {school.name} at BMUN 63 this year! Here,
-        you can view and edit your registration information for the conference,
-        edit your delegate roster, and find answers to frequently asked
-        questions. To edit your information, just click on any of the
-        highlighted fields and add your changes. Also, <span className="bold">
-        don't forget to save any information you update!</span></p>
+        <p>We are very excited to see {school.name} at BMUN 63 this year! Here,
+        you can view your registration information for the conference,
+        and find answers to frequently asked questions.</p>
         <br />
         <p><strong>Important Note:</strong> Please mail all checks to <strong>
         P.O. Box 4306 Berkeley, CA 94704-0306. If you have any other further
@@ -68,7 +43,6 @@ var AdvisorProfileView = React.createClass({
         Under-Secretary General of External Relations, 63rd Session.</p>
         <form id="welcomepage">
           <div className="tablemenu header">
-            Dont forget to save!
           </div>
           <div id="welcomeinfocontainer" className="table-container">
             <table id="welcomeinfo" className="table highlight-cells">
@@ -78,19 +52,13 @@ var AdvisorProfileView = React.createClass({
               <tr>
                 <td className="fieldLabel">First Name</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('first_name')}
-                  />
+                  {user.first_name}
                 </td>
               </tr>
               <tr>
                 <td className="fieldLabel">Last Name</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('last_name')}
-                  />
+                  {user.last_name}
                 </td>
               </tr>
               <tr>
@@ -99,37 +67,25 @@ var AdvisorProfileView = React.createClass({
               <tr>
                 <td className="fieldLabel">Name</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('school_name')}
-                  />
+                  {school.name}
                 </td>
               </tr>
               <tr>
                 <td className="fieldLabel">Address</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('school_address')}
-                  />
+                  {school.address}
                 </td>
               </tr>
               <tr>
                 <td className="fieldLabel">City</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('school_city')}
-                  />
+                  {school.city}
                 </td>
               </tr>
               <tr>
                 <td className="fieldLabel">Zip</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('school_zip_code')}
-                  />
+                  {school.zip_code}
                 </td>
               </tr>
               <tr>
@@ -138,39 +94,38 @@ var AdvisorProfileView = React.createClass({
               <tr>
                 <td className="fieldLabel">Program Type</td>
                 <td className="field">
-                  <input
-                    type="radio"
-                    name="program_type"
-                    valueLink={this.linkState('program_type')}
-                    checked={ProgramTypes.CLASS === school.program_type}
-                  />
-                  <label>Class</label>
-                  &nbsp;&nbsp;&nbsp;&nbsp;
-                  <input
-                    type="radio"
-                    name="program_type"
-                    valueLink={this.linkState('program_type')}
-                    checked={ProgramTypes.CLUB === school.program_type}
-                  />
-                  <label>Club</label>
+                  {school.program_type === 1 ? 'Club' : 'Class'}
                 </td>
               </tr>
               <tr>
                 <td className="fieldLabel">Times Attended</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('times_attended')}
-                  />
+                  {school.times_attended}
                 </td>
               </tr>
               <tr>
-                <td className="fieldLabel">Delegation Size</td>
+                <td className="fieldLabel">Number of Beginner Delegates</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('delegation_size')}
-                  />
+                  {school.beginner_delegates}
+                </td>
+              </tr>
+              <tr>
+                <td className="fieldLabel">Number of Intermediate Delegates</td>
+                <td className="field">
+                  {school.intermediate_delegates}
+                </td>
+              </tr>
+              <tr>
+                <td className="fieldLabel">Number of Advanced Delegates</td>
+                <td className="field">
+                  {school.advanced_delegates}
+                </td>
+              </tr>
+              <tr>
+                <td className="fieldLabel">Number of Spanish Speaking
+                Delegates</td>
+                <td className="field">
+                  {school.spanish_speaking_delegates}
                 </td>
               </tr>
               <tr>
@@ -179,28 +134,19 @@ var AdvisorProfileView = React.createClass({
               <tr>
                 <td className="fieldLabel">Name</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('primary_name')}
-                  />
+                  {school.primary_name}
                 </td>
               </tr>
               <tr>
                 <td className="fieldLabel">Email</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('primary_email')}
-                  />
+                  {school.primary_email}
                 </td>
               </tr>
               <tr>
                 <td className="fieldLabel">Phone</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('primary_phone')}
-                  />
+                  {school.primary_phone}
                 </td>
               </tr>
               <tr>
@@ -209,34 +155,24 @@ var AdvisorProfileView = React.createClass({
               <tr>
                 <td className="fieldLabel">Name</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('secondary_name')}
-                  />
+                  {school.secondary_name}
                 </td>
               </tr>
               <tr>
                 <td className="fieldLabel">Email</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('secondary_email')}
-                  />
+                  {school.secondary_email}
                 </td>
               </tr>
               <tr>
                 <td className="fieldLabel">Phone</td>
                 <td className="field">
-                  <input
-                    type="text"
-                    valueLink={this.linkState('secondary_phone')}
-                  />
+                  {school.secondary_phone}
                 </td>
               </tr>
             </table>
           </div>
           <div className="tablemenu footer">
-            Dont forget to save!
           </div>
         </form>
         <br />
