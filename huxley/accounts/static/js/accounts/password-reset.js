28f52c03f019b535b078953c3fd4bf0454806cc3
Kunal Mehta <k.mehta@berkeley.edu>
2014-08-16 11:23:05 -0700

    Clean up dead code in huxley.accounts.
    This removes a bunch of the templates, template tags, and views
    in the accounts app. It leaves login-as-user and logout, just
    in case. It also scraps all the static files, but moves the
    SCSS to www (because it's still used).
    
    It removes the useless `login` method from the model, and renames
    the test to `test_user.py`.
    
    Test Plan: Login and logout of the app, verify it works.

--- huxley/accounts/static/js/accounts/password-reset.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    $(document).on('submit', '#password-reset', function() {
-        var uri = $(this).attr('action');
-        ContentManager.loadNewContent(uri, $(this).serializeArray());
-        return false;
-    });
-});
