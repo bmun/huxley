39f32cbe1d8540d85af845ec288625ea22f39f8d
Pranay Patil <patilpranay@gmail.com>
2014-08-24 01:47:18 -0700

    issue #267. removed old css from "Change Password!" button.

--- huxley/www/static/js/huxley/components/ChangePasswordView.js
+++ huxley/www/static/js/huxley/components/ChangePasswordView.js
@@ -16,130 +16,131 @@ var ChangePasswordView = React.createClass({
   mixins: [React.addons.LinkedStateMixin],
 
   propTypes: {
-    isVisible: React.PropTypes.bool.isRequired,
-    onSuccess: React.PropTypes.func.isRequired
+	isVisible: React.PropTypes.bool.isRequired,
+	onSuccess: React.PropTypes.func.isRequired
   },
 
   getInitialState: function() {
-    return {
-      message: '',
-      success: false,
-      loading: false,
-      currentPassword: '',
-      newPassword: '',
-      newPassword2: '',
-    };
+	return {
+	  message: '',
+	  success: false,
+	  loading: false,
+	  currentPassword: '',
+	  newPassword: '',
+	  newPassword2: '',
+	};
   },
 
   componentWillReceiveProps: function(nextProps) {
-    this.setState(this.getInitialState());
+	this.setState(this.getInitialState());
   },
 
   render: function() {
-    if (!this.props.isVisible) {
-      return <div />;
-    }
+	if (!this.props.isVisible) {
+	  return <div />;
+	}
 
-    return (
-      <div
-        id="changepassword-container"
-        className="change-password rounded-bottom transparent">
-        <form
-          id="changepassword"
-          onSubmit={this._handleSubmit}>
-          <div className="input">
-            <label htmlFor="oldpassword">Current Password</label>
-            <input
-              type="password"
-              className="rounded-small"
-              valueLink={this.linkState('currentPassword')}
-            />
-          </div>
-          <div className="input">
-            <label htmlFor="newpassword">New Password</label>
-            <input
-              type="password"
-              className="rounded-small"
-              valueLink={this.linkState('newPassword')}
-            />
-          </div>
-          <div className="input">
-            <label htmlFor="newpassword">New Password (again)</label>
-            <input
-              type="password"
-              className="rounded-small"
-              valueLink={this.linkState('newPassword2')}
-            />
-          </div>
-          <div className="rounded-small topbarbutton">
-            <Button
-              type="submit"
-              loading={this.state.loading}>
-              Change Password!
-            </Button>
-          </div>
-        </form>
-        {this.renderMessage()}
-      </div>
-    );
+	return (
+	  <div
+		id="changepassword-container"
+		className="change-password rounded-bottom transparent">
+		<form
+		  id="changepassword"
+		  onSubmit={this._handleSubmit}>
+		  <div className="input">
+			<label htmlFor="oldpassword">Current Password</label>
+			<input
+			  type="password"
+			  className="rounded-small"
+			  valueLink={this.linkState('currentPassword')}
+			/>
+		  </div>
+		  <div className="input">
+			<label htmlFor="newpassword">New Password</label>
+			<input
+			  type="password"
+			  className="rounded-small"
+			  valueLink={this.linkState('newPassword')}
+			/>
+		  </div>
+		  <div className="input">
+			<label htmlFor="newpassword">New Password (again)</label>
+			<input
+			  type="password"
+			  className="rounded-small"
+			  valueLink={this.linkState('newPassword2')}
+			/>
+		  </div>
+		  <div className="rounded-small topbarbutton">
+			<Button
+			  color="green"
+			  size="small"
+			  loading={this.state.loading}>
+			  Change Password!
+			</Button>
+		  </div>
+		</form>
+		{this.renderMessage()}
+	  </div>
+	);
   },
 
   renderMessage: function() {
-    if (!this.state.message) {
-      return null;
-    }
+	if (!this.state.message) {
+	  return null;
+	}
 
-    return (
-      <div id="message">
-        <label className={this.state.success ? 'success' : 'error'}>
-          {this.state.message}
-        </label>
-      </div>
-    );
+	return (
+	  <div id="message">
+		<label className={this.state.success ? 'success' : 'error'}>
+		  {this.state.message}
+		</label>
+	  </div>
+	);
   },
 
   onSuccess: function() {
-    setTimeout(this.props.onSuccess, 500);
+	setTimeout(this.props.onSuccess, 500);
   },
 
   _handleSubmit: function(event) {
-    if (this.state.newPassword != this.state.newPassword2) {
-      this.setState({
-        message: 'Please enter the same password again',
-        success: false,
-      });
-    } else {
-      this.setState({loading: true});
-      $.ajax({
-        type: 'PUT',
-        url: '/api/users/me/password',
-        data: {
-          password: this.state.currentPassword,
-          new_password: this.state.newPassword,
-        },
-        success: this._handleSuccess,
-        error: this._handleError
-      }),
-      event.preventDefault();
-    }
+	if (this.state.newPassword != this.state.newPassword2) {
+	  this.setState({
+		message: 'Please enter the same password again',
+		success: false,
+	  });
+	} else {
+	  this.setState({loading: true});
+	  $.ajax({
+		type: 'PUT',
+		url: '/api/users/me/password',
+		data: {
+		  password: this.state.currentPassword,
+		  new_password: this.state.newPassword,
+		},
+		success: this._handleSuccess,
+		error: this._handleError
+	  }),
+	  event.preventDefault();
+	}
   },
 
   _handleSuccess: function(data, status, jqXHR) {
-    this.setState({
-      success: true,
-      message: 'Success',
-      currentPassword: '',
-      newPassword: '',
-      newPassword2: '',
-    }, this.onSuccess);
+	this.setState({
+	  success: true,
+	  message: 'Success',
+	  currentPassword: '',
+	  newPassword: '',
+	  newPassword2: '',
+	}, this.onSuccess);
   },
 
   _handleError: function(jqXHR, status, error) {
-    var response = jqXHR.responseJSON;
-    this.setState({
-      message: response.detail,
-      success: false,
-    });
+	var response = jqXHR.responseJSON;
+	this.setState({
+	  message: response.detail,
+	  success: false,
+	});
   },
 });
 
