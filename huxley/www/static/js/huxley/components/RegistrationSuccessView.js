1951b29b2541cf49200ef01c8a87ef7d176860c0
Pranay Patil <patilpranay@gmail.com>
2014-08-27 11:23:27 -0700

    issue #277. The spacing was fixed.

--- huxley/www/static/js/huxley/components/RegistrationSuccessView.js
+++ huxley/www/static/js/huxley/components/RegistrationSuccessView.js
@@ -13,66 +13,66 @@ var NavLink = require('./NavLink');
 var OuterView = require('./OuterView');
 
 var RegistrationSuccessView = React.createClass({
-  render: function() {
-	return (
-	  <OuterView>
-		<div class="letter">
-		  <h1>Thank You</h1>
-		  <p>
-			Thank you for registering for BMUN 63! Country assignments will be
-			made available soon in your Huxley account. A summary of fees is
-			listed below:
-		  </p>
-		  <h3>School Registration Fee</h3>
-		  <ul>
-			<li>$50</li>
-		  </ul>
-		  <h3>Delegate Registration Fee</h3>
-		  <ul>
-			<li>$50</li>
-		  </ul>
-		  <h3>Payment Instructions</h3>
-		  <p>
-			<strong>
-			  We ask that you compile delegate fees into ONE check
-			</strong>
-			&nbsp;&#8212;
-			please do not have your students mail individual checks.
-		  </p>
-		  <p>
-			Please mail all checks out to:
-		  </p>
-		  <div class="address">
-			<p><strong>Berkeley Model United Nations</strong>
-			  <br />
-			  P.O. Box #4306
-			  <br />
-			  Berkeley, CA 94704-0306
-			</p>
-		  </div>
-		  <p>
-			If you have any questions or concerns, please feel free to contact
-			me at <a href="mailto:info@bmun.org">info@bmun.org</a>. We look
-			forward to seeing you in March!
-		  </p>
-		  <p>Sincerely,</p>
-		  <p class="sender">
-			<strong>Hee Soo Kim</strong>
-			<br />
-			<span class="subtext">
-			  Under-Secretary General of External Relations
-			  <br />
-			  Berkeley Model United Nations, 63nd Session
-			</span>
-		  </p>
-		</div>
-		<hr />
-		<NavLink direction="right" href="/login">
-		  Proceed to Login
-		</NavLink>
-	  </OuterView>
-	);
-  }
+	render: function() {
+		return (
+			<OuterView>
+				<div class="letter">
+					<h1>Thank You</h1>
+					<p>
+						Thank you for registering for BMUN 63! Country assignments will be
+						made available soon in your Huxley account. A summary of fees is
+						listed below:
+					</p>
+					<h3>School Registration Fee</h3>
+					<ul>
+						<li>$50</li>
+					</ul>
+					<h3>Delegate Registration Fee</h3>
+					<ul>
+						<li>$50</li>
+					</ul>
+					<h3>Payment Instructions</h3>
+					<p>
+						<strong>
+							We ask that you compile delegate fees into ONE check
+						</strong>
+						&nbsp;&#8212;
+						please do not have your students mail individual checks.
+					</p>
+					<p>
+						Please mail all checks out to:
+					</p>
+					<div class="address">
+						<p><strong>Berkeley Model United Nations</strong>
+							<br />
+							P.O. Box #4306
+							<br />
+							Berkeley, CA 94704-0306
+						</p>
+					</div>
+					<p>
+						If you have any questions or concerns, please feel free to contact
+						me at <a href="mailto:info@bmun.org">info@bmun.org</a>. We look
+						forward to seeing you in March!
+					</p>
+					<p>Sincerely,</p>
+					<p class="sender">
+						<strong>Hee Soo Kim</strong>
+						<br />
+						<span class="subtext">
+							Under-Secretary General of External Relations
+							<br />
+							Berkeley Model United Nations, 63nd Session
+						</span>
+					</p>
+				</div>
+				<hr />
+				<NavLink direction="right" href="/login">
+					Proceed to Login
+				</NavLink>
+			</OuterView>
+		);
+	}
 });
 
 module.exports = RegistrationSuccessView;
