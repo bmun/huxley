b5ddc6b776e4af525a27812d75499e90166b919b
Trevor Dowds <tdowds@berkeley.edu>
2014-08-27 17:16:22 -0700

    (#46) Add initial logging settings.
    Closes #281.

--- huxley/settings/logging.py
+++ huxley/settings/logging.py
@@ -1,2 +1,20 @@
 # Copyright (c) 2011-2014 Berkeley Model United Nations. All rights reserved.
 # Use of this source code is governed by a BSD License (see LICENSE).
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'huxley': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
