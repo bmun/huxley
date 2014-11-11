10d77b9ee576053fb486255981e47f0f5e70afa9
Trevor Dowds <tdowds@berkeley.edu>
2014-11-10 22:44:00 -0800

    #261 incorporated Assignments.update_assignments in admin view.

--- huxley/core/admin/assignment.py
+++ huxley/core/admin/assignment.py
@@ -8,8 +8,7 @@ from django.contrib import admin
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 
-from huxley.core.models import Assignment, Committee, Country
-from xlrd import open_workbook
+from huxley.core.models import Assignment, Committee, Country, School
 
 
 class AssignmentAdmin(admin.ModelAdmin):
@@ -30,31 +29,20 @@ class AssignmentAdmin(admin.ModelAdmin):
         return assignments
 
     def load(self, request):
-        '''Loads Committees, Countries and Assignments and sweeps old ones.'''
+        '''Loads new Assignments.'''
         assignments = request.FILES
-        print(assignments)
-        s = open_workbook(assignments['excel'].name).sheet_by_index(0)
+        reader = csv.reader(assignments['csv'])
 
-        country_range = s.nrows
-        committee_range = 22
+        new_assignments = []
+        for row in reader:
+            if (len(row[4]) < 2): #ignore the first row because of headers
+                committee = Committee.objects.get(name=row[2])
+                country = Country.objects.get(name=row[3])
+                school = School.objects.get(name=row[0])
+                assignment = (committee.id, country.id, school.id)
+                new_assignments.append(assignment)
 
-        Country.objects.all().delete()
-        for row in range(3, country_range):
-            Country.objects.get_or_create(name=s.cell(row, 0).value, special=(True if row > 154 else False))
-
-        Committee.objects.all().delete()
-        for col in range(1, committee_range):
-            Committee.objects.get_or_create(name=s.cell(1, col).value, full_name=s.cell(2, col).value, delegation_size=(1 if s.cell(0, col).value == 'SINGLE' else 2), special=(True if (col > 13 and col != 21) else False))
-
-        Assignment.objects.all().delete()
-        for row in range(3, country_range):
-            for col in range(1, committee_range):
-                if s.cell(row, col).value:
-                    country = Country.objects.get(name=s.cell(row, 0).value)
-                    committee = Committee.objects.get(name=s.cell(1, col).value)
-                    assignment = Assignment(committee=committee, country=country)
-                    assignment.save()
-
+        Assignment.update_assignments(new_assignments)
         return HttpResponseRedirect(reverse('admin:core_assignment_changelist'))
 
     def get_urls(self):
