2ec20c96ef70b124e76f1c0246e9a1413fba8013
Pranay Patil <patilpranay@gmail.com>
2014-11-15 14:17:52 -0800

    Redirects to admin panel if you are logged in as a superuser.
    Resolves #332, Closes #366.

--- huxley/www/views.py
+++ huxley/www/views.py
@@ -3,12 +3,18 @@
 
 import json
 
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
+
 from huxley.api.serializers import UserSerializer
 from huxley.core.constants import ContactGender, ContactType, ProgramTypes
 from huxley.utils.shortcuts import render_template
 
 
 def index(request):
+    if request.user.is_superuser:
+        return redirect(reverse('admin:index'))
+
     user_dict = {};
     if request.user.is_authenticated():
         user_dict = UserSerializer(request.user).data
