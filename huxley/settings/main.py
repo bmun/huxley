c3a154a255d5044424ae1dbc0ae867621c1237ab
Trevor Dowds <tdowds@berkeley.edu>
2015-01-16 15:00:30 -0800

    #46 corrected exception logger middleware.

--- huxley/settings/main.py
+++ huxley/settings/main.py
@@ -74,7 +74,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'huxley.settings.middlewares.ExceptionLoggerMiddleware',
+    'huxley.core.middlewares.ExceptionLoggerMiddleware',
 )
 
 AUTHENTICATION_BACKENDS = (
