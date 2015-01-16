2f5c95d09793f0ca4d8ce3b02db3b13068810602
Trevor Dowds <tdowds@berkeley.edu>
2015-01-15 16:47:52 -0800

    (#46) Add middleware for logging exceptions.
    Closes #379.

--- huxley/settings/main.py
+++ huxley/settings/main.py
@@ -74,6 +74,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'huxley.core.middlewares.ExceptionLoggerMiddleware',
 )
 
 AUTHENTICATION_BACKENDS = (
