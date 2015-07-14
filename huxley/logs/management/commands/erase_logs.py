d35c014715ed1c8fe2a043293f7c9fbe9f6ee4d4
Trevor Dowds <tdowds@berkeley.edu>
2015-07-13 22:09:12 -0700

    Added script to erase old LogEntries from the database.

---
+++ huxley/logs/management/commands/erase_logs.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2011-2015 Berkeley Model United Nations. All rights reserved.
+# Use of this source code is governed by a BSD License (see LICENSE).
+
+from datetime import datetime, timedelta
+from django.core.management.base import BaseCommand
+
+from huxley.logs.models import LogEntry
+
+class Command(BaseCommand):
+    args = '<days> (optional).'
+    help = 'Deletes all log entries after a specified number of days (defaults to 30).'
+
+    def handle(self, *args, **kwargs):
+        if args:
+            try:
+                days = int(args[0])
+            except ValueError as e:
+                raise e
+        else:
+            days = 30
+
+        cutoff = datetime.now() - timedelta(days=days)
+        records = LogEntry.objects.filter(timestamp__lt=cutoff)
+        records.delete()
+
+        return
+
