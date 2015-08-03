7b8fa1baa2176d3d2ed2c7203f79ce40ec174965
Nathaniel Parke <nathaniel.parke@gmail.com>
2015-08-02 18:37:12 -0700

    fixes migration errors

---
+++ huxley/core/migrations/0002_auto_20150802_1835.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='school',
+            name='prefers_alternative',
+        ),
+        migrations.RemoveField(
+            model_name='school',
+            name='prefers_bilingual',
+        ),
+        migrations.RemoveField(
+            model_name='school',
+            name='prefers_crisis',
+        ),
+        migrations.RemoveField(
+            model_name='school',
+            name='prefers_press_corps',
+        ),
+        migrations.RemoveField(
+            model_name='school',
+            name='prefers_specialized_regional',
+        ),
+        migrations.AddField(
+            model_name='school',
+            name='assignments_finalized',
+            field=models.BooleanField(default=False),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='school',
+            name='committeepreferences',
+            field=models.ManyToManyField(to='core.Committee'),
+            preserve_default=True,
+        ),
+    ]
