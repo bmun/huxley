5b2a1a98d767afb1985fbf672d0f547a738c56a0
Trevor Dowds <tdowds@berkeley.edu>
2016-04-03 14:21:41 -0700

    fixed empty field checking on front end after updating DRF.

--- huxley/www/static/js/huxley/components/RegistrationView.js
+++ huxley/www/static/js/huxley/components/RegistrationView.js
@@ -507,10 +507,10 @@ var RegistrationView = React.createClass({
 
   renderSchoolError: function(field) {
     if (this.state.errors.school &&
-        this.state.errors.school[0][field]) {
+        this.state.errors.school[field]) {
       return (
         <label className="hint error">
-          {this.state.errors.school[0][field]}
+          {this.state.errors.school[field]}
         </label>
       );
     }
