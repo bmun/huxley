f3fa725b60a027214e8021e8679141de17b67c66
Michael McDonald <m_j_mcdonald@comcast.net>
2016-06-09 23:21:05 -0700

    Updated error message

--- huxley/core/admin/assignment.py
+++ huxley/core/admin/assignment.py
@@ -2,11 +2,13 @@
 # Use of this source code is governed by a BSD License (see LICENSE).
 
 import csv
+import Tkinter, tkMessageBox
 
 from django.conf.urls import patterns, url
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
+from django.core.exceptions import FieldError
 
 from huxley.core.models import Assignment, Committee, Country, School
 
@@ -39,30 +41,69 @@ class AssignmentAdmin(admin.ModelAdmin):
         '''Loads new Assignments.'''
         assignments = request.FILES
         reader = csv.reader(assignments['csv'])
+        successful_rows = []
+        failed_rows = []
 
         def get_model(model, name, cache):
             if not name in cache:
                 cache[name] = model.objects.get(name=name)
             return cache[name]
 
-        def generate_assigments(reader):
+        def get_bad_columns(row):
+            bad_columns = set()
+            if not Committee.objects.filter(name=row[1]).exists():
+                bad_columns.add(1)
+            if not Country.objects.filter(name=row[2]).exists():
+                bad_columns.add(2)
+            if not School.objects.filter(name=row[0]).exists():
+                bad_columns.add(0)
+            return bad_columns
+
+        def create_assignments(reader):
             committees = {}
             countries = {}
             schools = {}
+            assigned = set()
+
             for row in reader:
                 if (row[0]=='School' and row[1]=='Committee' and row[2]=='Country'):
                     continue # skip the first row if it is a header
-                committee = get_model(Committee, row[1], committees)
-                country = get_model(Country, row[2], countries)
-                school = get_model(School, row[0], schools)
+                
+                while len(row) < 3:
+                    row.append("") # extends the row to have the minimum proper num of columns
+
+                bad_columns = get_bad_columns(row)
+                already_assigned = (row[1], row[2]) in assigned
+                
                 if len(row) < 4:
                     rejected = False # allow for the rejected field to be null
                 else:
                     rejected = (row[3].lower() == 'true') # use the provided value if admin provides it
-                yield (committee.id, country.id, school.id, rejected)
+                
+                if not bad_columns and not already_assigned:
+                    committee = get_model(Committee, row[1], committees)
+                    country = get_model(Country, row[2], countries)
+                    school = get_model(School, row[0], schools)
+                    assigned.add((row[1], row[2]))
+                    successful_rows.append((committee.id, country.id, school.id, rejected))
+                else:
+                    for col in bad_columns:
+                        row[col] = row[col] + ' - INVALID'
+                    
+                    if already_assigned:
+                        row[1] = row[1] + ' - ALREADY ASSIGNED'
+                        row[2] = row[2] + ' - ALREADY ASSIGNED'
+                    failed_rows.append(str((row[0], row[1], row[2], str(rejected))))
 
-        Assignment.update_assignments(generate_assigments(reader))
-        return HttpResponseRedirect(reverse('admin:core_assignment_changelist'))
+        create_assignments(reader)
+        if not failed_rows:
+            Assignment.update_assignments(successful_rows)
+            return HttpResponseRedirect(reverse('admin:core_assignment_changelist'))
+        else:
+            messages.error(request, 
+                'Assignments aborted.\nThese are the rows that failed with identification of the cells at fault:\n' + ', \n'.join(failed_rows))
+            return HttpResponseRedirect(reverse('admin:core_assignment_changelist'))
+
 
     def get_urls(self):
         urls = super(AssignmentAdmin, self).get_urls()
