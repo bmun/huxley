5811a14642f44b106146d394c3bd4a75e70c6657
Jake Moskowitz <jmoskowitz12@berkeley.edu>
2016-08-03 22:13:40 -0500

    implemented editing feature

--- huxley/www/static/js/huxley/components/AdvisorRosterView.js
+++ huxley/www/static/js/huxley/components/AdvisorRosterView.js
@@ -5,110 +5,114 @@
  * @jsx React.DOM
  */
  
- 'use strict';
+'use strict';
  
- var $ = require('jquery');
- var Modal = require('react-modal');
- var React = require('react');
- var ReactRouter = require('react-router');
+var $ = require('jquery');
+var Modal = require('react-modal');
+var React = require('react');
+var ReactRouter = require('react-router');
  
- var AssignmentStore = require('../stores/AssignmentStore');
- var Button = require('./Button');
- var CommitteeStore = require('../stores/CommitteeStore');
- var CountryStore = require('../stores/CountryStore');
- var CurrentUserStore = require('../stores/CurrentUserStore');
- var DelegateStore = require('../stores/DelegateStore');
- var CurrentUserActions = require('../actions/CurrentUserActions');
- var InnerView = require('./InnerView');
- var TextInput = require('./TextInput');
- var _handleChange = require('../utils/_handleChange');
+var AssignmentStore = require('../stores/AssignmentStore');
+var Button = require('./Button');
+var CommitteeStore = require('../stores/CommitteeStore');
+var CountryStore = require('../stores/CountryStore');
+var CurrentUserStore = require('../stores/CurrentUserStore');
+var DelegateStore = require('../stores/DelegateStore');
+var CurrentUserActions = require('../actions/CurrentUserActions');
+var InnerView = require('./InnerView');
+var TextInput = require('./TextInput');
+var _handleChange = require('../utils/_handleChange');
  
- const customStyles = {
-   content : {
-     top                   : '50%',
-     left                  : '50%',
-     right                 : 'auto',
-     bottom                : 'auto',
-     marginRight           : '-50%',
-     transform             : 'translate(-50%, -50%)'
-   }
- };
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
  
- var AdvisorRosterView = React.createClass({
-   mixins: [
-     ReactRouter.History,
-   ],
+var AdvisorRosterView = React.createClass({
+  mixins: [
+    ReactRouter.History,
+  ],
  
-   getInitialState: function() {
-     return {
-       assignments: [],
-       delegates: [],
-       loading: false,
-       adding_delegate: false,
-       add_name: '',
-       add_email: '',
-     };
-   },
+  getInitialState: function() {
+    return {
+      assignments: [],
+      delegates: [],
+      loading: false,
+      adding_delegate: false,
+      editing_delegate: false,
+      add_name: '',
+      add_email: '',
+      edit_name: '',
+      edit_email: '',
+      delegate_id: 0,
+    };
+  },
  
-   componentWillMount: function() {
-     var user = CurrentUserStore.getCurrentUser();
+  componentWillMount: function() {
+    var user = CurrentUserStore.getCurrentUser();
  
-     AssignmentStore.getAssignments(user.school.id, function(assignments) {
-       this.setState({assignments: assignments.filter(
-         function(assignment) {
-           return !assignment.rejected
-         }
-       )});
-     }.bind(this));
+    AssignmentStore.getAssignments(user.school.id, function(assignments) {
+      this.setState({assignments: assignments.filter(
+        function(assignment) {
+          return !assignment.rejected
+        }
+      )});
+    }.bind(this));
  
-     DelegateStore.getDelegates(user.school.id, function(delegates) {
-       this.setState({delegates: delegates});
-     }.bind(this));
+    DelegateStore.getDelegates(user.school.id, function(delegates) {
+      this.setState({delegates: delegates});
+    }.bind(this));
  
-     Modal.setAppElement('body')
-   },
+    Modal.setAppElement('body')
+  },
  
-   render: function() {
-     return (
-       <InnerView>
-         <h2>Roster</h2>
-         <p>
-           Here you can add your schools delegates to your roster.
-           Any comments that chairs have about your delegate will appear here.
-         </p>
-         <form>
-           <div className="tablemenu header" />
-           <div className="table-container">
-             <table className="table highlight-cells">
-               <thead>
-                 <tr>
-                   <th>Delegate</th>
-                   <th>Email</th>
-                   <th>Summary</th>
-                   <th>Delete Delegate</th>
-                 </tr>
-               </thead>
-               <tbody>
-                 {this.renderRosterRows()}
-               </tbody>
-             </table>
-           </div>
-         </form>
-         <Button
-           color="green"
-           onClick={this.openModal}
-           loading={this.state.loading}>
-           Add Delegate
-         </Button>
-         <Modal
-           isOpen={this.state.adding_delegate}
-           onAfterOpen={this.afterOpenModal}
-           onRequestClose={this._handleSubmit}
-           style={customStyles}>
-           <button onClick={this.closeModal}>close</button>
-           <h2 ref="subtitle">Add Delegate</h2>
-           <form>
-             <TextInput
+  render: function() {
+    return (
+      <InnerView>
+        <h2>Roster</h2>
+        <p>
+          Here you can add your schools delegates to your roster.
+          Any comments that chairs have about your delegate will appear here.
+        </p>
+        <form>
+          <div className="tablemenu header" />
+          <div className="table-container">
+            <table className="table highlight-cells">
+              <thead>
+                <tr>
+                  <th>Delegate</th>
+                  <th>Email</th>
+                  <th>Summary</th>
+                  <th>Delete Delegate</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.renderRosterRows()}
+              </tbody>
+            </table>
+          </div>
+        </form>
+        <Button
+          color="green"
+          onClick={this.openAddModal}
+          loading={this.state.loading}>
+          Add Delegate
+        </Button>
+        <Modal
+          isOpen={this.state.adding_delegate}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this._handleAddDelegate}
+          style={customStyles}>
+          <button onClick={this.closeModal.bind(this, 'add')}>close</button>
+          <h2 ref="subtitle">Add Delegate</h2>
+          <form>
+            <TextInput
               placeholder="Name"
               onChange={_handleChange.bind(this, 'add_name')}
               value={this.state.add_name}
@@ -120,114 +124,183 @@
               value={this.state.add_email}
             />
             <br></br>
-            <Button onClick={this._handleSubmit}
+            <Button onClick={this._handleAddDelegate}
             color="green"
             loading={this.state.loading}>
             Add</Button>
-           </form>
-         </Modal>
-       </InnerView>
-     );
-   },
+          </form>
+        </Modal>
+        <Modal
+          isOpen={this.state.editing_delegate}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this._handleEditDelegateSuccess}
+          style={customStyles}>
+          <button onClick={this.closeModal.bind(this, 'edit')}>close</button>
+          <h2 ref="subtitle">Edit Delegate</h2>
+          <form>
+            <TextInput
+              placeholder="Name"
+              onChange={_handleChange.bind(this, 'edit_name')}
+              value={this.state.edit_name}
+            />
+            <br></br>
+            <TextInput
+               placeholder="Email"
+               onChange={_handleChange.bind(this, 'edit_email')}
+               value={this.state.edit_email}
+            />
+            <br></br>
+            <Button onClick={this._handleEditDelegate}
+            color="green"
+            loading={this.state.loading}>
+            Save</Button>
+          </form>
+        </Modal>
+      </InnerView>
+    );
+  },
  
-   renderRosterRows: function() {
-     var committees = this.state.committees;
-     var countries = this.state.countries;
-     console.dir(this.state.delegates[0])
-     return this.state.delegates.map(function(delegate) {
-       return (
-         <tr>
-           <td>{delegate.name}</td>
-           <td>{delegate.email}</td>
-           <td>{delegate.summary}</td>
-           <td>
-             <Button color="red"
-                     size="small"
-                     onClick={this._handleDeleteDelegate.bind(this, delegate)}>
-               Delete
-             </Button>
-           </td>
-         </tr>
-       )
-     }.bind(this));
-   },
+  renderRosterRows: function() {
+    var committees = this.state.committees;
+    var countries = this.state.countries;
+    console.dir(this.state.delegates[0])
+    return this.state.delegates.map(function(delegate) {
+      return (
+        <tr>
+          <td>{delegate.name}</td>
+          <td>{delegate.email}</td>
+          <td>{delegate.summary}</td>
+          <td>
+            <Button color="blue"
+                    size="small"
+                    onClick={this.openEditModal.bind(this, delegate)}>
+              Edit
+            </Button>
+            <Button color="red"
+                    size="small"
+                    onClick={this._handleDeleteDelegate.bind(this, delegate)}>
+              Delete
+            </Button>
+          </td>
+        </tr>
+      )
+    }.bind(this));
+  },
  
-   _handleAddDelegate: function() {
-     this.setState({adding_delegate: true});
-   },
- 
-   openModal: function() {
-     this.setState({adding_delegate: true});
-   },
- 
-   afterOpenModal: function() {
-     // references are now sync'd and can be accessed.
-     this.refs.subtitle.style.color = '#f00';
-   },
- 
-   closeModal: function() {
-     this.setState({adding_delegate: false});
-   },
- 
-   _handleDeleteDelegate: function(delegate) {
-     this.setState({loading: true});
-     $.ajax ({
-       type: 'DELETE',
-       url: '/api/delegates/'+delegate.id,
-       success: this._handleDelegateDeleteSuccess,
-       error: this._handleError,
-     });
-   },
- 
-   _handleDelegateDeleteSuccess: function(data, status, jqXHR) {
-     var delegates = this.state.delegates
-     delegates = delegates.filter(function (delegate) {
-        return delegate.id != jqXHR.responseJSON.delegate.id;
-     });
-     this.setState({delegates: delegates})
-     this.setState({loading: false});
-     this.history.pushState(null, '/advisor/roster');
-   },
- 
-   _handleSubmit: function(data) {
-     this.setState({loading: true});
-     var user = CurrentUserStore.getCurrentUser();
-     $.ajax({
-       type: 'POST',
-       url: '/api/delegates',
-       data: JSON.stringify({
-         name: this.state.add_name,
-         email: this.state.add_email,
-         school: user.school.id
-       }),
-       success: this._handleSuccess,
-       error: this._handleError,
-       dataType: 'json',
-       contentType: 'application/json'
-     });
-     event.preventDefault();
-   },
- 
-   _handleSuccess: function(data, status, jqXHR) {
-     var delegates = this.state.delegates;
-     delegates.push(data);
-     this.setState({adding_delegate: false});
-     this.setState({loading: false});
+  openAddModal: function() {
+    this.setState({adding_delegate: true});
+  },
 
-     this.history.pushState(null, '/advisor/roster');
-   },
+  openEditModal: function(delegate) {
+    this.setState({editing_delegate: true});
+    this.setState({delegate_id: delegate.id});
+    this.setState({edit_name: delegate.name});
+    this.setState({edit_email: delegate.email});
+  },
  
-   _handleError: function(jqXHR, status, error) {
-     var response = jqXHR.responseJSON;
-     if (!response) {
-       return;
-     }
+  afterOpenModal: function() {
+    // references are now sync'd and can be accessed.
+    this.refs.subtitle.style.color = '#f00';
+  },
  
-     this.setState({
-       loading: false
-     }.bind(this));
-   }
+  closeModal: function(modal) {
+    if (modal === "add") {
+      this.setState({adding_delegate: false});
+    } else if (modal === "edit") {
+      this.setState({editing_delegate: false});
+    }
+  },
  
- });
+  _handleDeleteDelegate: function(delegate) {
+    this.setState({loading: true});
+    $.ajax ({
+      type: 'DELETE',
+      url: '/api/delegates/'+delegate.id,
+      success: this._handleDelegateDeleteSuccess,
+      error: this._handleError,
+    });
+  },
+
+  _handleAddDelegate: function(data) {
+    this.setState({loading: true});
+    var user = CurrentUserStore.getCurrentUser();
+    $.ajax({
+      type: 'POST',
+      url: '/api/delegates/',
+      data: JSON.stringify({
+        name: this.state.add_name,
+        email: this.state.add_email,
+        school: user.school.id
+      }),
+      success: this._handleAddDelegateSuccess,
+      error: this._handleError,
+      dataType: 'json',
+      contentType: 'application/json'
+    });
+    event.preventDefault();
+  },
+
+  _handleEditDelegate: function(delegate) {
+    var user = CurrentUserStore.getCurrentUser();
+    this.setState({loading: true});
+    $.ajax ({
+      type: 'PATCH',
+      url: '/api/delegates/'+this.state.delegate_id,
+      data: JSON.stringify({
+        name: this.state.edit_name,
+        email: this.state.edit_email,
+        school: user.school.id
+      }),
+      success: this._handleEditDelegateSuccess,
+      error: this._handleError,
+      dataType: 'json',
+      contentType: 'application/json'
+    });
+    event.preventDefault();
+  },
  
- module.exports = AdvisorRosterView;
+  _handleDelegateDeleteSuccess: function(data, status, jqXHR) {
+    var delegates = this.state.delegates;
+    delegates = delegates.filter(function (delegate) {
+        return delegate.id != jqXHR.responseJSON.delegate.id;
+    });
+    this.setState({delegates: delegates});
+    this.setState({loading: false});
+    this.history.pushState(null, '/advisor/roster');
+  },
+ 
+  _handleAddDelegateSuccess: function(data, status, jqXHR) {
+    var delegates = this.state.delegates;
+    delegates.push(data);
+    this.setState({adding_delegate: false});
+    this.setState({loading: false});
+    this.history.pushState(null, '/advisor/roster');
+  },
+
+  _handleEditDelegateSuccess: function(data, status, jqXHR) {
+    var delegates = this.state.delegates;
+    delegates = delegates.filter(function (delegate) {
+        return delegate.id != jqXHR.responseJSON.delegate.id;
+    });
+    this.setState({editing_delegate: false});
+    this.setState({delegates: delegates});
+    this.setState({loading: false});
+    this.history.pushState(null, '/advisor/roster');
+  },
+ 
+  _handleError: function(jqXHR, status, error) {
+    console.log("error");
+    console.log(error);
+    var response = jqXHR.responseJSON;
+    if (!response) {
+      return;
+    }
+ 
+    this.setState({
+      loading: false
+    }.bind(this));
+  }
+ 
+});
+ 
+module.exports = AdvisorRosterView;
