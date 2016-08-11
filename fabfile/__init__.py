2c5088edc848325834c3f440dfd445c46cefa455
m-j-mcdonald <m_j_mcdonald@comcast.net>
2016-08-11 11:11:02 -0700

    Introduce python autoformatter (#496)
    This adds a python auto formatter as discussed in #472. I introduced a command 'fab format' to run the auto formatter, which also gets run as part of submit. As an example of what the auto formatter might do, look at how the code of init.py and git.py was modified.
    
    Resolves #472.

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -3,12 +3,13 @@
 
 from os.path import abspath, dirname, join
 
-from fabric.api import abort, env, local, settings, task
+from fabric.api import abort, hide, env, execute, local, settings, task
 from fabric.contrib.console import confirm
+
+from yapf.yapflib.yapf_api import FormatFile
 
 from . import dependencies, migrations, pr, test
 from .utils import git, ui
-
 
 env.huxley_root = abspath(dirname(dirname(__file__)))
 env.js_root = join(env.huxley_root, 'huxley/www/static/js')
@@ -22,7 +23,8 @@ def feature(branch_name=None):
         dependencies.check()
         migrations.check()
     else:
-        print ui.error('No branch name given. Usage: fab feature:<branch_name>')
+        print ui.error(
+            'No branch name given. Usage: fab feature:<branch_name>')
 
 
 @task
@@ -37,8 +39,30 @@ def update():
 
 
 @task
+def format():
+    '''Format and commit python files committed on the current feature branch'''
+    diff_list = git.diff_name_only()
+    py_diff_list = [pyfile for pyfile in diff_list if pyfile.endswith('.py')]
+
+    if confirm('Review formatting changes? (Select no to approve all)'):
+
+        for pyfile in py_diff_list:
+            print('Changes:\n' + FormatFile(pyfile, print_diff=True)[0])
+            if confirm('Accept changes to %s?' % pyfile):
+                FormatFile(pyfile, in_place=True)
+    else:
+        for pyfile in py_diff_list:
+            FormatFile(pyfile, in_place=True)
+    print ui.info('Formatting complete')
+
+    if len(local('git status --porcelain', capture=True)) > 0:
+        local('git commit -am "Ran autoformatter"')
+
+
+@task
 def submit(remote='origin', skip_tests=False):
     '''Push the current feature branch and create/update pull request.'''
+    format()
     if not skip_tests:
         with settings(warn_only=True):
             if not test.run():
@@ -61,20 +85,23 @@ def submit(remote='origin', skip_tests=False):
         local('hub pull-request -b bmun:master -h %s -f' % current_branch)
         print ui.success('Pull request successfully issued.')
     else:
-        print ui.success('Branch successfully pushed. Go to GitHub to issue a pull request.')
+        print ui.success(
+            'Branch successfully pushed. Go to GitHub to issue a pull request.')
 
 
 @task
 def finish(branch_name=None, remote='origin'):
     '''Delete the current feature branch.'''
-    prompt = ui.warning('This will delete your local and remote topic branches. '
-                        'Make sure your pull request has been merged or closed. '
-                        'Are you sure you want to finish this branch?')
+    prompt = ui.warning(
+        'This will delete your local and remote topic branches. '
+        'Make sure your pull request has been merged or closed. '
+        'Are you sure you want to finish this branch?')
     if not confirm(prompt):
         abort('Branch deletion canceled.')
 
-    print ui.success('Branch %s successfully cleaned up.' % git.cleanup(branch_name,
-                                                                   remote))
+    print ui.success('Branch %s successfully cleaned up.' %
+                     git.cleanup(branch_name, remote))
+
 
 try:
     from deploy import deploy, restart
