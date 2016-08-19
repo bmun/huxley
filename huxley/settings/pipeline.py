3d17cf3c6d4242a86e1930286297a7346c535a4d
Kunal Mehta <k.mehta@berkeley.edu>
2016-08-18 20:05:15 -0700

    Switch to Webpack for JS. (#497)
    Ditches pipeline+browserify for JS. Removes the browserify dependencies, pipeline settings, and adjusts the deploy command in fabric to build the JS before running collectstatic.
    
    Resolves #387.

--- huxley/settings/pipeline.py
+++ huxley/settings/pipeline.py
@@ -1,19 +1,13 @@
 # Copyright (c) 2011-2015 Berkeley Model United Nations. All rights reserved.
 # Use of this source code is governed by a BSD License (see LICENSE).
 
-from os.path import join
-
-from .roots import JS_ROOT, PROJECT_ROOT
-
 
 PIPELINE = {
     'COMPILERS': (
         'huxley.utils.pipeline.PySCSSCompiler',
-        'huxley.utils.pipeline.BrowserifyCompiler',
     ),
 
     'CSS_COMPRESSOR': 'pipeline.compressors.cssmin.CSSMinCompressor',
-    'JS_COMPRESSOR': 'pipeline.compressors.uglifyjs.UglifyJSCompressor',
 
     'STYLESHEETS': {
         'huxley': {
@@ -24,19 +18,4 @@ PIPELINE = {
             'output_filename': 'css/huxley.css',
         },
     },
-
-    'JAVASCRIPT': {
-        'huxley': {
-            'source_filenames': (
-                'js/huxley.browserify.js',
-            ),
-            'output_filename': 'js/huxley.js',
-        },
-    },
-
-    'UGLIFYJS_BINARY': join(PROJECT_ROOT, 'node_modules/.bin/uglifyjs'),
-    'UGLIFYJS_ARGUMENTS': '--compress --mangle --screw-ie8',
-
-    'BROWSERIFY_BINARY': join(PROJECT_ROOT, 'node_modules/.bin/browserify'),
-    'BROWSERIFY_ARGUMENTS': '-t babelify -t [detachkify -relativeTo %s]' % JS_ROOT,
 }
