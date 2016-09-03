97ea4e0ef2f584e9b3f7dfb33cecbaacff53a265
Trevor Dowds <tdowds@berkeley.edu>
2016-09-03 12:05:58 -0700

    Updated registration closed page.

--- huxley/www/static/js/huxley/components/RegistrationClosedView.js
+++ huxley/www/static/js/huxley/components/RegistrationClosedView.js
@@ -7,21 +7,37 @@
 
 var React = require('react');
 
+var ConferenceContext = require('components/ConferenceContext');
 var NavLink = require('components/NavLink');
 var OuterView = require('components/OuterView');
 
 var RegistrationClosedView = React.createClass({
+  contextTypes: {
+    conference: React.PropTypes.shape(ConferenceContext)
+  },
+
   render: function() {
+    var conference = this.context.conference;
     return (
       <OuterView>
         <div className="letter">
           <h1>I'm Sorry</h1>
+          {conference.registration_waitlist ?
           <p>
-            Thank you for your interest in participating in the sixty-fourth
-            session of Berkeley Model United Nations. Registration is now closed
+            Thank you for your interest in participating in
+            BMUN {conference.session}. Registration is now closed
             as we have already reached our maximum capacity. We hope to see you
             at the next BMUN session!
           </p>
+          :
+          <p>
+            Thank you for your interest in BMUN {conference.session}.
+            Registration will be open on September 12th at 8 AM.
+            If you have any questions please
+            contact <a href="mailto:info@bmun.org">info@bmun.org</a>.
+            We hope to see you at BMUN 65!
+          </p>
+          }
         </div>
         <hr />
         <NavLink direction="right" href="/login">
