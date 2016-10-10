6680216b5be68485aa1032e1de13d8a162196bb4
Kunal Mehta <k.mehta@berkeley.edu>
2016-10-09 17:49:38 -0700

    Add ES2017 and object spread features to Babel.
    This will allow us to add trailing commas to functions and use the object spread operator.

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,8 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['es2015', 'react'],
+          plugins: ['transform-object-rest-spread'],
+          presets: ['es2015', 'es2017', 'react'],
         },
       },
     ],
