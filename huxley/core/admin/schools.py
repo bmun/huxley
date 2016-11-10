3f5c23fc9f4c1de5afa94c02ede2bef85308e99b
Trevor Dowds <tdowds@berkeley.edu>
2016-11-09 22:07:42 -0800

    Added committee preferences column when downloading assignment preferences from admin portal (#538)
    * added committee preferences to assignment preference csv
    
    * Ran autoformatter
    
    * messed up .gitignore change.
    
    * fixed tests.
    
    * Ran autoformatter
    
    * more specific .gitignore

--- huxley/core/admin/schools.py
+++ huxley/core/admin/schools.py
@@ -9,9 +9,10 @@ from django.http import HttpResponse
 
 from huxley.core.models import School
 
+
 class SchoolAdmin(admin.ModelAdmin):
 
-    readonly_fields = ('balance',)
+    readonly_fields = ('balance', )
 
     def calc_balance(self, obj):
         return obj.balance()
@@ -57,38 +58,39 @@ class SchoolAdmin(admin.ModelAdmin):
         ])
 
         for school in School.objects.all().order_by('name'):
-            writer.writerow([unicode(field).encode('utf8') for field in [
-                school.id,
-                school.name,
-                school.address,
-                school.city,
-                school.state,
-                school.zip_code,
-                school.country,
-                school.primary_name,
-                school.primary_gender,
-                school.primary_email,
-                school.primary_phone,
-                school.primary_type,
-                school.secondary_name,
-                school.secondary_gender,
-                school.secondary_email,
-                school.secondary_phone,
-                school.secondary_type,
-                school.program_type,
-                school.times_attended,
-                school.international,
-                school.waitlist,
-                school.beginner_delegates,
-                school.intermediate_delegates,
-                school.advanced_delegates,
-                school.spanish_speaking_delegates,
-                school.chinese_speaking_delegates,
-                school.registration_comments,
-                school.fees_owed,
-                school.fees_paid,
-                school.modified_at.isoformat(),
-            ]])
+            writer.writerow([unicode(field).encode('utf8')
+                             for field in [
+                                 school.id,
+                                 school.name,
+                                 school.address,
+                                 school.city,
+                                 school.state,
+                                 school.zip_code,
+                                 school.country,
+                                 school.primary_name,
+                                 school.primary_gender,
+                                 school.primary_email,
+                                 school.primary_phone,
+                                 school.primary_type,
+                                 school.secondary_name,
+                                 school.secondary_gender,
+                                 school.secondary_email,
+                                 school.secondary_phone,
+                                 school.secondary_type,
+                                 school.program_type,
+                                 school.times_attended,
+                                 school.international,
+                                 school.waitlist,
+                                 school.beginner_delegates,
+                                 school.intermediate_delegates,
+                                 school.advanced_delegates,
+                                 school.spanish_speaking_delegates,
+                                 school.chinese_speaking_delegates,
+                                 school.registration_comments,
+                                 school.fees_owed,
+                                 school.fees_paid,
+                                 school.modified_at.isoformat(),
+                             ]])
 
         return schools
 
@@ -96,60 +98,48 @@ class SchoolAdmin(admin.ModelAdmin):
         ''' Returns a CSV file containing the current set of
             Schools registered with all of its fields. '''
         schools = HttpResponse(content_type='text/csv')
-        schools['Content-Disposition'] = 'attachment; filename="preferences.csv"'
+        schools[
+            'Content-Disposition'] = 'attachment; filename="preferences.csv"'
         writer = csv.writer(schools)
 
         writer.writerow([
-                "Name",
-                "Assignments Requested",
-                "Beginners",
-                "Intermediates",
-                "Advanced",
-                "Spanish Speakers",
-                "Chinese Speakers",
-                "Country 1",
-                "Country 2",
-                "Country 3",
-                "Country 4",
-                "Country 5",
-                "Country 6",
-                "Country 7",
-                "Country 8",
-                "Country 9",
-                "Country 10",
-                "Registration Comments"
-                ])
+            "Name", "Assignments Requested", "Beginners", "Intermediates",
+            "Advanced", "Spanish Speakers", "Chinese Speakers", "Country 1",
+            "Country 2", "Country 3", "Country 4", "Country 5", "Country 6",
+            "Country 7", "Country 8", "Country 9", "Country 10",
+            "Committee Preferences", "Registration Comments"
+        ])
 
         for school in School.objects.all().order_by('name'):
-            countryprefs = [c for c in school.countrypreferences.all().order_by('countrypreference')]
+            countryprefs = [c
+                            for c in school.countrypreferences.all().order_by(
+                                'countrypreference')]
             countryprefs += [''] * (10 - len(countryprefs))
+            committeeprefs = [', '.join(
+                [c.name for c in school.committeepreferences.all()])]
 
-            writer.writerow([unicode(field).encode('utf8') for field in [
-                school.name,
-                school.beginner_delegates + school.intermediate_delegates + school.advanced_delegates,
-                school.beginner_delegates,
-                school.intermediate_delegates,
-                school.advanced_delegates,
-                school.spanish_speaking_delegates,
-                school.chinese_speaking_delegates]] +
-                countryprefs +
-                [unicode(school.registration_comments).encode('utf8')
-            ])
+            writer.writerow(
+                [unicode(field).encode('utf8')
+                 for field in [
+                     school.name, school.beginner_delegates +
+                     school.intermediate_delegates + school.advanced_delegates,
+                     school.beginner_delegates, school.intermediate_delegates,
+                     school.advanced_delegates,
+                     school.spanish_speaking_delegates,
+                     school.chinese_speaking_delegates
+                 ]] + countryprefs + committeeprefs + [unicode(
+                     school.registration_comments).encode('utf8')])
 
         return schools
 
     def get_urls(self):
         urls = super(SchoolAdmin, self).get_urls()
-        urls += patterns('',
-            url(
-                r'info',
+        urls += patterns(
+            '',
+            url(r'info',
                 self.admin_site.admin_view(self.info),
-                name='core_school_info',
-            ),
-            url(
-                r'preferences',
+                name='core_school_info', ),
+            url(r'preferences',
                 self.admin_site.admin_view(self.preferences),
-                name='core_school_preferences'
-            )
-        )
+                name='core_school_preferences'))
         return urls
