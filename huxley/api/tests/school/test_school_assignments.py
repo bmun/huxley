706e0014176cf83dcad3efbbdc6f99e7ee9dec6b
Kunal Mehta <k.mehta@berkeley.edu>
2016-12-27 18:24:53 -0500

    Merge SchoolAssignments into AssignmentList (#547)

--- huxley/api/tests/school/test_school_assignments.py
+++ huxley/api/tests/school/test_school_assignments.py
@@ -1,80 +1,10 @@
 # Copyright (c) 2011-2015 Berkeley Model United Nations. All rights reserved.
 # Use of this source code is governed by a BSD License (see LICENSE).
 
-from huxley.api.tests import ListAPITestCase, UpdateAPITestCase
+from huxley.api.tests import UpdateAPITestCase
 from huxley.core.models import Assignment, School
 from huxley.utils.test import (TestCommittees, TestCountries, TestSchools,
                                TestUsers)
-
-
-class SchoolAssignmentsGetTestCase(ListAPITestCase):
-    url_name = 'api:school_assignments'
-    is_resource = True
-
-    def setUp(self):
-        self.user = TestUsers.new_user(username='regular', password='user')
-        self.school = TestSchools.new_school(user=self.user)
-        self.country = TestCountries.new_country()
-        self.committee1 = TestCommittees.new_committee()
-        self.committee2 = TestCommittees.new_committee()
-        self.assignment1 = Assignment.objects.create(
-            committee=self.committee1,
-            country=self.country,
-            school=self.school,
-        )
-        self.assignment2 = Assignment.objects.create(
-            committee=self.committee2,
-            country=self.country,
-            school=self.school,
-        )
-
-    def test_anonymous_user(self):
-        '''It rejects a request from an anonymous user.'''
-        response = self.get_response(self.school.id)
-        self.assertNotAuthenticated(response)
-
-    def test_advisor(self):
-        '''It returns the assignments for the school's advisor.'''
-        self.client.login(username='regular', password='user')
-
-        response = self.get_response(self.school.id)
-        self.assert_assignments_equal(response)
-
-    def test_other_user(self):
-        '''It rejects a request from another user.'''
-        user2 = TestUsers.new_user(username='another', password='user')
-        TestSchools.new_school(user=user2)
-        self.client.login(username='another', password='user')
-
-        response = self.get_response(self.school.id)
-        self.assertPermissionDenied(response)
-
-    def test_superuser(self):
-        '''It returns the assignments for a superuser.'''
-        TestUsers.new_superuser(username='test', password='user')
-        self.client.login(username='test', password='user')
-
-        response = self.get_response(self.school.id)
-        self.assert_assignments_equal(response)
-
-    def assert_assignments_equal(self, response):
-        '''Assert that the response contains the assignments in order.'''
-        self.assertEqual(response.data, [
-            {
-                'id': self.assignment1.id,
-                'country': self.country.id,
-                'committee': self.committee1.id,
-                'school': self.school.id,
-                'rejected': self.assignment1.rejected,
-            },
-            {
-                'id': self.assignment2.id,
-                'country': self.country.id,
-                'committee': self.committee2.id,
-                'school': self.school.id,
-                'rejected': self.assignment2.rejected,
-            },
-        ])
 
 
 class SchoolAssignmentsFinalizeTestCase(UpdateAPITestCase):
--- huxley/api/views/assignment.py
+++ huxley/api/views/assignment.py
@@ -1,26 +1,34 @@
 # Copyright (c) 2011-2015 Berkeley Model United Nations. All rights reserved.
 # Use of this source code is governed by a BSD License (see LICENSE).
 
-from rest_framework import generics, status
+from rest_framework import generics
 from rest_framework.authentication import SessionAuthentication
-from rest_framework.response import Response
 
-from huxley.api.permissions import IsSchoolAssignmentAdvisorOrSuperuser, IsSuperuserOrReadOnly
+from huxley.api import permissions
 from huxley.api.serializers import AssignmentSerializer
 from huxley.core.models import Assignment
 
 
-class AssignmentList(generics.CreateAPIView):
+class AssignmentList(generics.ListCreateAPIView):
     authentication_classes = (SessionAuthentication,)
-    queryset = Assignment.objects.all()
-    permission_classes = (IsSuperuserOrReadOnly,)
+    permission_classes = (permissions.AssignmentListPermission,)
     serializer_class = AssignmentSerializer
+
+    def get_queryset(self):
+        queryset = Assignment.objects.all()
+        query_params = self.request.GET
+
+        school_id = query_params.get('school_id', None)
+        if school_id:
+            queryset = queryset.filter(school_id=school_id)
+
+        return queryset
 
 
 class AssignmentDetail(generics.RetrieveUpdateAPIView):
     authentication_classes = (SessionAuthentication,)
     queryset = Assignment.objects.all()
-    permission_classes = (IsSchoolAssignmentAdvisorOrSuperuser,)
+    permission_classes = (permissions.IsSchoolAssignmentAdvisorOrSuperuser,)
     serializer_class = AssignmentSerializer
 
     def put(self, request, *args, **kwargs):
