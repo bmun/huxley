7da69e28f7ca0f40603718009b32b1fe4afd406d
Michael McDonald <m_j_mcdonald@comcast.net>
2016-12-28 17:10:37 -0800

    Fixing merge

--- huxley/api/views/delegate.py
+++ huxley/api/views/delegate.py
@@ -42,7 +42,24 @@ class DelegateDetail(generics.RetrieveUpdateDestroyAPIView):
 
 
 class DelegateDetail(generics.RetrieveUpdateDestroyAPIView):
-    authentication_classes = (SessionAuthentication,)
+    authentication_classes = (SessionAuthentication, )
     queryset = Delegate.objects.all()
-    permission_classes = (IsSchoolDelegateAdvisorOrSuperuser,)
+    permission_classes = (IsSchoolDelegateAdvisorOrSuperuser, )
     serializer_class = DelegateSerializer
+
+
+class DelegateCommitteeDetail(generics.ListAPIView, ListUpdateModelMixin):
+    authentication_classes = (SessionAuthentication, )
+    queryset = Delegate.objects.all()
+    serializer_class = DelegateSerializer
+    permission_classes = (IsChairOrSuperuser, )
+
+    def get_queryset(self):
+        '''Filter schools by the given pk param.'''
+        committee_id = self.kwargs.get('pk', None)
+        if not committee_id:
+            raise Http404
+        return Delegate.objects.filter(assignment__committee_id=committee_id)
+
+    def patch(self, request, *args, **kwargs):
+        return self.list_update(request, partial=True, *args, **kwargs)
