0fbc4338328dbd0228183e80fe09c0c64ca31b31
Shayna Kothari <shayna.kothari@gmail.com>
2021-01-11 00:46:44 -0800

    packages updated, but site doesn't display

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,30 @@
-var path = require('path');
-var webpack = require('webpack');
+var path = require("path");
+var webpack = require("webpack");
 
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
-var package = require('./package.json');
+var package = require("./package.json");
 
-var JS_ROOT = path.join(__dirname, 'huxley/www/js');
-var STATIC_ROOT = path.join(__dirname, 'huxley/www/static/js');
+var JS_ROOT = path.join(__dirname, "huxley/www/js");
+var STATIC_ROOT = path.join(__dirname, "huxley/www/static/js");
 
 const marked = require("marked");
 const renderer = new marked.Renderer();
 
 var plugins = [
-  new webpack.EnvironmentPlugin([
-    'NODE_ENV',
-  ]),
-  new ExtractTextPlugin('huxley.css'),
-  new webpack.optimize.CommonsChunkPlugin({
-    name: 'vendor',
-    filename: 'vendor.js',
-  }),
+  new webpack.EnvironmentPlugin(["NODE_ENV"]),
+  new MiniCssExtractPlugin({ filename: "huxley.css" }),
 ];
-if (process.env.NODE_ENV === 'production') {
-  plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-      },
-      mangle: true,
-    })
-  );
-}
 
 module.exports = {
   entry: {
-    huxley: path.join(JS_ROOT, 'entry.js'),
+    huxley: path.join(JS_ROOT, "entry.js"),
     vendor: Object.keys(package.dependencies),
   },
   output: {
     path: STATIC_ROOT,
-    filename: '[name].js',
+    filename: "[name].js",
   },
   module: {
     rules: [
@@ -47,41 +32,72 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            plugins: ['transform-object-rest-spread'],
-            presets: ['es2015', 'es2017', 'react'],
-          }
+            plugins: ["@babel/plugin-proposal-object-rest-spread"],
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
         },
       },
       {
         test: /\.less$/,
         exclude: /node_modules/,
-        use: ExtractTextPlugin.extract({
-          use: [
-            {loader: 'css-loader'},
-            {loader: 'less-loader'},
-          ],
-          fallback: 'style-loader',
-        }),
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "less-loader",
+        ],
       },
       {
         test: /\.md$/,
         use: [
-          {loader: "html-loader"},
+          { loader: "html-loader" },
           {
             loader: "markdown-loader",
             options: {
               pedantic: true,
-              renderer
-            }
-          }
+              renderer,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        exclude: /node_modules/,
+        use: [
+          {loader: "url-loader"},
         ]
       },
     ],
   },
   plugins: plugins,
   resolve: {
-    modules: ['node_modules', JS_ROOT],
+    modules: ["node_modules", JS_ROOT],
+    fallback: {
+      "path": require.resolve("path-browserify"),
+      "crypto": require.resolve("crypto-browserify"),
+      "buffer": require.resolve("buffer/"),
+      "stream": require.resolve("stream-browserify")
+    }
   },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ]
+  },
+  // optimization: {
+  //   runtimeChunk: "single", // enable "runtime" chunk
+  //   splitChunks: {
+  //     cacheGroups: {
+  //       vendor: {
+  //         test: /[\\/]node_modules[\\/]/,
+  //         chunks: "all",
+  //       },
+  //     },
+  //   },
+  // },
+  mode: process.env.NODE_ENV,
 };
