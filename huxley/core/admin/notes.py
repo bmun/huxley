9e8fc70b9350f184dda42303b8b8c81ed80df422
Srisai Nachuri <srisainachuri@berkeley.edu>
2021-09-16 02:38:10 -0700

    Added sheets integration

--- huxley/core/admin/notes.py
+++ huxley/core/admin/notes.py
@@ -3,11 +3,16 @@
 
 import csv
 
+from django.conf import settings
 from django.conf.urls import url
 from django.contrib import admin
-from django.http import HttpResponse
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+
 
 from googleapiclient.discovery import build
+from google.oauth2 import service_account
+
 
 from huxley.core.models import Note
 
@@ -17,14 +22,14 @@ class NotesAdmin(admin.ModelAdmin):
     def get_rows(self):
         '''Return the rows of data for huxley notes.'''
         rows = []
-        rows.append(['is_chair', 'sender_receiver',
-                    'timestamp', 'msg', 'sender'])
+        rows.append(['timestamp', 'summary', 'sender', 'sender is chair?', 'recipient',
+                    'message'])
 
         for note in Note.objects.all().order_by(
                 'timestamp'):
             rows.append([
                 str(item) for item in [
-                    note.is_chair, note, note.timestamp, note.msg, note.sender
+                    note.timestamp, note, note.sender, note.is_chair, note.recipient, note.msg
                 ]
             ])
 
@@ -41,9 +46,42 @@ class NotesAdmin(admin.ModelAdmin):
 
         return notes
 
+    def sheets(self, request):
+        if settings.SHEET_ID:
+            SHEET_RANGE = 'Notes!A1:F'
+            # Store credentials
+            creds = service_account.Credentials.from_service_account_file(
+                settings.SERVICE_ACCOUNT_FILE, scopes=settings.SCOPES)
+
+            data = self.get_rows()
+
+            body = {
+                'values': data,
+            }
+
+            service = build('sheets', 'v4', credentials=creds)
+            response = service.spreadsheets().values().clear(
+                spreadsheetId=settings.SHEET_ID,
+                range=SHEET_RANGE,
+            ).execute()
+
+            response = service.spreadsheets().values().update(
+                spreadsheetId=settings.SHEET_ID,
+                range=SHEET_RANGE,
+                valueInputOption='USER_ENTERED',
+                body=body).execute()
+
+        return HttpResponseRedirect(
+            reverse('admin:core_note_changelist'))
+
     def get_urls(self):
         return super(NotesAdmin, self).get_urls() + [
             url(r'list',
                 self.admin_site.admin_view(self.list),
                 name='core_note_list'),
+            url(
+                r'sheets',
+                self.admin_site.admin_view(self.sheets),
+                name='core_note_sheets',
+            ),
         ]
