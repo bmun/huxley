456d758b74ac1310841a83b598eb77e3b4408d51
Eric Wang <erwang01@gmail.com>
2021-12-11 23:10:35 -0800

    Redirect on logout and login

--- huxley/www/js/components/Huxley.js
+++ huxley/www/js/components/Huxley.js
@@ -59,6 +59,21 @@ require("css/IEWarning.less");
 
 class Huxley extends React.Component {
 
+  componentDidMount() {
+    CurrentUserStore.addListener(() => {
+      var user = CurrentUserStore.getCurrentUser();
+      if (User.isAnonymous(user)) {
+        history.redirect("/login");
+      } else if (User.isAdvisor(user)) {
+        history.redirect("/advisor/profile");
+      } else if (User.isChair(user)) {
+        history.redirect("/chair/attendance");
+      } else if (User.isDelegate(user)) {
+        history.redirect("/delegate/profile");
+      }
+    });
+  }
+  
   render() {
     var user = CurrentUserStore.getCurrentUser();
     if (User.isAnonymous(user)) {
