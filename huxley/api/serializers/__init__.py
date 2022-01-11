93fdf2ae920435b08334e5e2dbe62460f7f263fe
Srisai Nachuri <srisainachuri@berkeley.edu>
2022-01-10 23:25:42 -0800

    updating copyrights to 2022

--- huxley/api/serializers/__init__.py
+++ huxley/api/serializers/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2011-2015 Berkeley Model United Nations. All rights reserved.
+# Copyright (c) 2011-2022 Berkeley Model United Nations. All rights reserved.
 # Use of this source code is governed by a BSD License (see LICENSE).
 
 from .assignment import AssignmentSerializer, AssignmentNestedSerializer
