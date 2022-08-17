6790c6c0e74427114138e6d52878a71fde505c29
Sora Kanosue <sorakanosue@berkeley.edu>
2022-08-17 23:46:16 +0200

    Implemented updateCustomerFromSchool, did sanity test

--- invoice_automation/src/model/School.py
+++ invoice_automation/src/model/School.py
@@ -18,6 +18,8 @@ class School:
         School's primary phone number, optional secondary phone number
     address: Address
         School's billing address
+    id: str
+        Id of QB Customer corresponding to this school
     """
 
     def __init__(self, schoolName: str, email: str, phoneNumbers: List[str], address: Address) -> None:
@@ -25,3 +27,4 @@ class School:
         self.email = email
         self.phoneNumbers = phoneNumbers
         self.address = address
+        self.id = None
